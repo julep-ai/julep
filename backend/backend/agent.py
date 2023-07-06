354e0c904069e5aada1c6a2bda2add91cffc9de1
Dmitry Paramonov <asmatic075@gmail.com>
2023-07-06 08:07:02 +0300

    fix: Remove exception handling

--- backend/backend/agent.py
+++ backend/backend/agent.py
@@ -174,16 +174,12 @@ class SamanthaAgent(RespondAgent[SamanthaConfig]):
             yield cut_off_response
             return
 
-        try:
-            # Add belief information
-            belief = to_belief_chatml_msg(
-                get_matching_beliefs(
-                    mem + [dict(role="user", content=human_input)], 0.5
-                )
+        # Add belief information
+        belief = to_belief_chatml_msg(
+            get_matching_beliefs(
+                mem + [dict(role="user", content=human_input)], 0.5
             )
-
-        except BaseException as e:
-            print(e)
+        )
 
         mem.append(belief)
 
