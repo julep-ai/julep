23f0ee07b00fa75b37b8416042ee570471bfbf57
Dmitry Paramonov <asmatic075@gmail.com>
2023-07-06 14:22:03 +0300

    feat: Add access log

--- backend/backend/logger.py
+++ backend/backend/logger.py
@@ -1,4 +1,6 @@
+import pathlib
 import logging
+from logging.handlers import RotatingFileHandler
 
 
 logger = logging.getLogger(__name__)
@@ -6,4 +8,13 @@ h = logging.StreamHandler()
 fmt = logging.Formatter("[%(asctime)s/%(levelname)s] %(message)s")
 h.setFormatter(fmt)
 logger.addHandler(h)
+logger.setLevel(logging.DEBUG)
+
+
+access_logger = logging.getLogger("access")
+log_path = (pathlib.Path(__file__).parent / ".." / "access.log").resolve()
+handler = RotatingFileHandler(log_path, maxBytes=50 * 1024 * 1024, backupCount=10)
+formatter = logging.Formatter("[%(asctime)s/%(levelname)s] %(message)s")
+handler.setFormatter(formatter)
+access_logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
