3ee4f38e7021ba2a86837b5f5fb6a28a8b1066ce
Dmitry Paramonov <asmatic075@gmail.com>
2023-09-19 15:26:45 +0300

    feat: Add error handfling

--- memory/api/memory_api/env.py
+++ memory/api/memory_api/env.py
@@ -7,3 +7,7 @@ env.read_env()
 
 cozo_host: str = env.str("COZO_HOST")
 cozo_auth: str = env.str("COZO_AUTH", default=None)
+prediction_project: str = env.str("PREDICTION_PROJECT")
+prediction_endpoint_id: str = env.str("PREDICTION_ENDPOINT_ID")
+prediction_location: str = env.str("PREDICTION_LOCATION", default="us-central1")
+prediction_api_endpoint: str = env.str("PREDICTION_API_ENDPOINT", default="us-central1-aiplatform.googleapis.com")
--- memory/api/memory_api/web.py
+++ memory/api/memory_api/web.py
@@ -1,7 +1,7 @@
 import fire
 import uvicorn
 from fastapi import FastAPI
-from memory_api.routers import characters, sessions
+from memory_api.routers import characters, sessions, embedder, users
 from memory_api.routers.characters.db import init as characters_db_init
 from memory_api.routers.sessions.db import init as sessions_db_init
 from memory_api.routers.users.db import init as users_db_init
@@ -10,6 +10,8 @@ from memory_api.routers.users.db import init as users_db_init
 app = FastAPI()
 app.include_router(characters.router)
 app.include_router(sessions.router)
+app.include_router(embedder.router)
+app.include_router(users.router)
 
 
 def main(host="127.0.0.1", port="8000", backlog=4096, timeout_keep_alive=30, workers=None, log_level="info"):
