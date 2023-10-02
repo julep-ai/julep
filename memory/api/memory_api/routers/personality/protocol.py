82cb2e1e7e6e96c0755be9e290492af3bc42cbcb
Dmitry Paramonov <asmatic075@gmail.com>
2023-10-02 15:52:11 +0300

    feat: Add user assesment API

---
+++ memory/api/memory_api/routers/personality/protocol.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, field_validator, Field
+
+
+class Answer(BaseModel):
+    question_number: int = Field(..., alias="questionNumber")
+    answer_number: int = Field(..., alias="answerNumber")
+
+    @field_validator("answer_number")
+    @staticmethod
+    def check_answer(cls, answer_number: int) -> int:
+        if 1 <= answer_number <= 7:
+            return answer_number
+        raise ValueError("answer value must be between 1 and 7")
+
+
+class AnswersRequest(BaseModel):
+    answers: list[Answer]
