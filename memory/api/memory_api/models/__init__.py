d5d932b1a80dff928f201e36d8b6fb0a89a08063
Diwank Singh Tomer <diwank.singh@gmail.com>
2023-12-25 14:41:18 +0530

    test: Test user queries
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- memory/api/memory_api/models/__init__.py
+++ memory/api/memory_api/models/__init__.py
@@ -1,11 +1,13 @@
+from memory_api.clients.cozo import client
+
 from .agent.schema import init as agent_init
 from .entry.schema import init as entry_init
 from .session.schema import init as session_init
 from .user.schema import init as user_init
 
 
-def init():
-    agent_init()
-    entry_init()
-    session_init()
-    user_init()
+def init(client=client):
+    agent_init(client)
+    entry_init(client)
+    session_init(client)
+    user_init(client)
