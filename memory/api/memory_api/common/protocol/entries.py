459cadeb52ec303be8efab9eb1adca595a9a4d3e
Diwank Singh Tomer <diwank.singh@gmail.com>
2023-12-26 12:33:18 +0530

    feat: literal type for tokenizer
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- memory/api/memory_api/common/protocol/entries.py
+++ memory/api/memory_api/common/protocol/entries.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel, Field, computed_field
 
 EntrySource = Literal["api_request", "api_response"]
 Role = Literal["user", "assistant", "system"]
+Tokenizer = Literal["character_count"]
+
 
 class Entry(BaseModel):
     id: UUID = Field(alias="entry_id", default_factory=uuid4)
@@ -21,9 +23,7 @@ class Entry(BaseModel):
     @property
     def token_count(self) -> int:
         if self.tokenizer == "character_count":
-            return int(
-                len(self.content) // 3.5
-            )
+            return int(len(self.content) // 3.5)
 
         raise NotImplementedError(f"Unknown tokenizer: {self.tokenizer}")
 
