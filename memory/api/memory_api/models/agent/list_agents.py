f7b85309bb5a9c7fde1371fce7eb0c035a68ce4c
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-01-03 19:25:46 +0530

    feat: Update agent model
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- memory/api/memory_api/models/agent/list_agents.py
+++ memory/api/memory_api/models/agent/list_agents.py
@@ -2,14 +2,18 @@ def list_agents_query(limit: int = 100, offset: int = 0):
     return f"""
         ?[
             agent_id,
+            model,
             name,
             about,
             created_at,
+            updated_at,
         ] := *agents {{
                 agent_id,
+                model,
                 name,
                 about,
                 created_at,
+                updated_at,
             }}
         
         :limit {limit}
