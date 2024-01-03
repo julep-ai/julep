f1ded62ea81198c45186f8c20588f2302caf6353
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-01-03 19:44:55 +0530

    feat: Update user and agent protocols
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- memory/api/memory_api/routers/agents/protocol.py
+++ memory/api/memory_api/routers/agents/protocol.py
@@ -1,5 +1,16 @@
+from typing import Literal
 from uuid import uuid4
+
 from pydantic import BaseModel, UUID4, Field
+
+
+class AgentDefaultSettings(BaseModel):
+    temperature: float = 0.0
+    top_p: float = 1.0
+    repetition_penalty: float = 1.0
+    length_penalty: float = 1.0
+    presence_penalty: float = 0.0
+    frequency_penalty: float = 0.0
 
 
 class Tool(BaseModel):
@@ -7,16 +18,24 @@ class Tool(BaseModel):
     definition: str
 
 
+ModelType = Literal["julep-ai/samantha-1", "julep-ai/samantha-1-turbo"]
+
+
 class CreateAgentRequest(BaseModel):
     id: UUID4 = Field(default_factory=uuid4, alias="agent_id")
     name: str
     about: str
+    model: ModelType = "julep-ai/samantha-1-turbo"
+    default_settings: AgentDefaultSettings = Field(default_factory=AgentDefaultSettings)
     # instructions: list[str] | None = None
     # tools: list[Tool]
 
 
 class UpdateAgentRequest(BaseModel):
+    name: str
     about: str
+    model: ModelType = "julep-ai/samantha-1-turbo"
+    default_settings: AgentDefaultSettings = Field(default_factory=AgentDefaultSettings)
     # instructions: list[str] | None = None
     # tools: list[Tool]
 
@@ -25,8 +44,9 @@ class Agent(BaseModel):
     id: UUID4 = Field(default_factory=uuid4, alias="agent_id")
     name: str
     about: str
+    model: ModelType = "julep-ai/samantha-1-turbo"
+    default_settings: AgentDefaultSettings = Field(default_factory=AgentDefaultSettings)
     # instructions: list[str] | None = None
     # tools: list[Tool]
-    metadata: dict = Field(default={})
     created_at: float | None = None
     updated_at: float | None = None
