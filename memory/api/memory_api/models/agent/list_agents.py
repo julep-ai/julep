03d319db00222308a29ad6723b2d5f1475552542
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-01-04 16:25:51 +0530

    feat: Update openapi schema and update queries
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- memory/api/memory_api/models/agent/list_agents.py
+++ memory/api/memory_api/models/agent/list_agents.py
@@ -17,4 +17,6 @@ def list_agents_query(limit: int = 100, offset: int = 0):
             }}
         
         :limit {limit}
-        :offset {offset}"""
+        :offset {offset}
+        :sort -created_at
+    """
--- memory/api/memory_api/models/session/list_sessions.py
+++ memory/api/memory_api/models/session/list_sessions.py
@@ -1,28 +1,29 @@
 def list_sessions_query(limit: int = 100, offset: int = 0):
     return f"""
-    ?[
-        agent_id,
-        user_id,
-        session_id,
-        situation,
-        summary,
-        updated_at,
-        created_at,
-    ] :=
-        *sessions{{
-            session_id,
-            situation,
-            summary,
-            created_at,
-            updated_at: validity,
-            @ "NOW"
-        }},
-        *session_lookup{{
+        ?[
             agent_id,
             user_id,
             session_id,
-        }}, updated_at = to_int(validity)
+            situation,
+            summary,
+            updated_at,
+            created_at,
+        ] :=
+            *sessions{{
+                session_id,
+                situation,
+                summary,
+                created_at,
+                updated_at: validity,
+                @ "NOW"
+            }},
+            *session_lookup{{
+                agent_id,
+                user_id,
+                session_id,
+            }}, updated_at = to_int(validity)
 
-    :limit {limit}
-    :offset {offset}
-"""
+        :limit {limit}
+        :offset {offset}
+        :sort -created_at
+    """
