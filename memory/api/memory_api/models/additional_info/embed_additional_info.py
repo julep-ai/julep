9ad9ca2d44253ccfb6b0ba30c6880560853a5c8d
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-01-11 13:52:32 +0530

    feat: Add embed additional info query
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ memory/api/memory_api/models/additional_info/embed_additional_info.py
@@ -0,0 +1,32 @@
+from typing import Literal
+from uuid import UUID
+
+
+def embed_additional_info_snippets_query(
+    additional_info_id: UUID,
+    snipped_indices: list[int],
+    embeddings: list[list[float]],
+):
+    additional_info_id = str(additional_info_id)
+    assert len(snipped_indices) == len(embeddings)
+
+    records = "\n".join(
+        [
+            f'[to_uuid("{additional_info_id}"), {snippet_idx}, vec({json.dumps(embedding)})],'
+            for snippet_idx, embedding in zip(snipped_indices, embeddings)
+        ]
+    )
+
+    return f"""
+    {{
+        ?[additional_info_id, snippet_idx, embedding] <- [
+            {records}
+        ]
+
+        :update information_snippets {{
+            additional_info_id,
+            snippet_idx,
+            embedding,
+        }}
+        :returning
+    }}"""
