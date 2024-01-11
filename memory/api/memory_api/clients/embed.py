84386562d6c5a05ce08dc095b8da77ce906a69d4
Dmitry Paramonov <asmatic075@gmail.com>
2024-01-11 14:19:20 +0300

    feat: Embed additional info

--- memory/api/memory_api/clients/embed.py
+++ memory/api/memory_api/clients/embed.py
@@ -1,4 +1,22 @@
+import httpx
+from ..env import embedding_service_url
+
+
 async def embed(
     inputs: list[str],
 ) -> list[list[float]]:
-    raise NotImplementedError
+    async with httpx.AsyncClient(timeout=30) as client:
+        resp = await client.post(
+            embedding_service_url,
+            headers={
+                "Content-Type": "application/json",
+            },
+            json={
+                "inputs": "\n\n".join(inputs),
+                "normalize": True,
+                "truncate": False,
+            },
+        )
+        resp.raise_for_status()
+
+        return resp.json()
