a338f6a6daf4eacf5be0471cf8411b136280a07d
Dmitry Paramonov <asmatic075@gmail.com>
2024-01-19 11:34:05 +0300

    feat: Add agents methods

--- sdks/python/julep_ai/managers/agent.py
+++ sdks/python/julep_ai/managers/agent.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from typing import Optional
 from beartype import beartype
 from beartype.typing import Awaitable, List, Literal, TypedDict, Union
 
@@ -11,6 +12,15 @@ from ..api.types import (
     FunctionDef,
     Instruction,
     ResourceCreatedResponse,
+    ListAgentsResponse,
+    ResourceUpdatedResponse,
+    GetAgentAdditionalInfoResponse,
+    GetAgentToolsResponse,
+    CreateToolRequest,
+    AdditionalInfo,
+    Tool,
+    GetAgentMemoriesResponse,
+    Memory,
 )
 
 from .base import BaseManager
@@ -102,6 +112,147 @@ class BaseAgentsManager(BaseManager):
             additional_info=docs,
         )
 
+    def _list_items(
+        self, limit: Optional[int] = None, offset: Optional[int] = None
+    ) -> Union[ListAgentsResponse, Awaitable[ListAgentsResponse]]:
+        return self.api_client.list_agents(
+            limit=limit,
+            offset=offset,
+        )
+
+    def _delete(self, agent_id: Union[str, UUID]) -> Union[None, Awaitable[None]]:
+        assert is_valid_uuid4(agent_id), "id must be a valid UUID v4"
+        return self.api_client.delete_agent(agent_id=agent_id)
+
+    def _update(
+        self,
+        agent_id: Union[str, UUID],
+        about: Optional[str] = None,
+        instructions: Optional[Union[List[str], List[InstructionDict]]] = None,
+        name: Optional[str] = None,
+        model: Optional[str] = None,
+        default_settings: Optional[DefaultSettingsDict] = None,
+    ) -> Union[ResourceUpdatedResponse, Awaitable[ResourceUpdatedResponse]]:
+        assert is_valid_uuid4(agent_id), "id must be a valid UUID v4"
+
+        if instructions is not None:
+            instructions: List[Instruction] = [
+                Instruction(content=content, important=False)
+                if isinstance(content, str)
+                else Instruction(**content)
+                for content in instructions
+            ]
+
+        if default_settings is not None:
+            default_settings: AgentDefaultSettings = AgentDefaultSettings(
+                **default_settings
+            )
+
+        return self.api_client.update_agent(
+            agent_id=agent_id,
+            about=about,
+            instructions=instructions,
+            name=name,
+            model=model,
+            default_settings=default_settings,
+        )
+
+    def _get_additional_info(
+        self,
+        agent_id: Union[str, UUID],
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> Union[
+        GetAgentAdditionalInfoResponse, Awaitable[GetAgentAdditionalInfoResponse]
+    ]:
+        return self.api_client.get_agent_additional_info(
+            agent_id=agent_id,
+            limit=limit,
+            offset=offset,
+        )
+
+    def _create_additional_info(
+        self,
+        agent_id: Union[str, UUID],
+        doc: DocDict,
+    ) -> Union[ResourceCreatedResponse, Awaitable[ResourceCreatedResponse]]:
+        doc: CreateAdditionalInfoRequest = CreateAdditionalInfoRequest(**doc)
+
+        return self.api_client.create_agent_additional_info(
+            agent_id=agent_id,
+            request=doc,
+        )
+
+    def _delete_additional_info(
+        self, agent_id: Union[str, UUID], additional_info_id: Union[str, UUID]
+    ):
+        return self.api_client.delete_agent_additional_info(
+            agent_id=agent_id,
+            additional_info_id=additional_info_id,
+        )
+
+    def _get_tools(
+        self,
+        agent_id: Union[str, UUID],
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> Union[GetAgentToolsResponse, Awaitable[GetAgentToolsResponse]]:
+        return self.api_client.get_agent_tools(
+            agent_id=agent_id, limit=limit, offset=offset
+        )
+
+    def _create_tool(
+        self, agent_id: Union[str, UUID], tool: ToolDict
+    ) -> Union[ResourceCreatedResponse, Awaitable[ResourceCreatedResponse]]:
+        tool: CreateToolRequest = CreateToolRequest(**tool)
+
+        return self.api_client.create_agent_tool(
+            agent_id=agent_id,
+            request=tool,
+        )
+
+    def _update_tool(
+        self,
+        agent_id: Union[str, UUID],
+        tool_id: Union[str, UUID],
+        definition: FunctionDefDict,
+    ) -> Union[ResourceUpdatedResponse, Awaitable[ResourceUpdatedResponse]]:
+        definition: FunctionDef = FunctionDef(**definition)
+
+        return self.api_client.update_agent_tool(
+            agent_id=agent_id,
+            tool_id=tool_id,
+            definition=definition,
+        )
+
+    def _delete_tool(
+        self,
+        agent_id: Union[str, UUID],
+        tool_id: Union[str, UUID],
+    ):
+        return self.api_client.delete_agent_tool(
+            agent_id=agent_id,
+            tool_id=tool_id,
+        )
+
+    def _get_memories(
+        self,
+        agent_id: str,
+        query: str,
+        types: Optional[Union[str, List[str]]] = None,
+        user_id: Optional[str] = None,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> Union[GetAgentMemoriesResponse, Awaitable[GetAgentMemoriesResponse]]:
+        return self.api_client.get_agent_memories(
+            agent_id=agent_id,
+            query=query,
+            types=types,
+            user_id=user_id,
+            limit=limit,
+            offset=offset,
+        )
+
 
 class AgentsManager(BaseAgentsManager):
     @beartype
@@ -132,6 +283,148 @@ class AgentsManager(BaseAgentsManager):
             docs,
         )
 
+    @beartype
+    def list(
+        self,
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Agent]:
+        return self._list_items(
+            limit=limit,
+            offset=offset,
+        ).items
+
+    @beartype
+    def delete(self, agent_id: Union[str, UUID]):
+        return self._delete(agent_id=agent_id)
+
+    @beartype
+    def update(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        about: Optional[str] = None,
+        instructions: Optional[Union[List[str], List[InstructionDict]]] = None,
+        name: Optional[str] = None,
+        model: Optional[str] = None,
+        default_settings: Optional[DefaultSettingsDict] = None,
+    ) -> ResourceUpdatedResponse:
+        return self._update(
+            agent_id=agent_id,
+            about=about,
+            instructions=instructions,
+            name=name,
+            model=model,
+            default_settings=default_settings,
+        )
+
+    @beartype
+    def get_additional_info(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[AdditionalInfo]:
+        return self._get_additional_info(
+            agent_id=agent_id,
+            limit=limit,
+            offset=offset,
+        ).items
+
+    @beartype
+    def create_additional_info(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        doc: DocDict,
+    ) -> ResourceCreatedResponse:
+        return self._create_additional_info(
+            agent_id=agent_id,
+            doc=doc,
+        )
+
+    @beartype
+    def delete_additional_info(
+        self, agent_id: Union[str, UUID], additional_info_id: Union[str, UUID]
+    ):
+        return self._delete_additional_info(
+            agent_id=agent_id,
+            additional_info_id=additional_info_id,
+        )
+
+    @beartype
+    def get_tools(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Tool]:
+        return self._get_tools(
+            agent_id=agent_id,
+            limit=limit,
+            offset=offset,
+        ).items
+
+    @beartype
+    def create_tool(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        tool: ToolDict,
+    ) -> ResourceCreatedResponse:
+        return self._create_tool(
+            agent_id=agent_id,
+            tool=tool,
+        )
+
+    @beartype
+    def update_tool(
+        self,
+        agent_id: Union[str, UUID],
+        tool_id: Union[str, UUID],
+        *,
+        definition: FunctionDefDict,
+    ) -> ResourceUpdatedResponse:
+        return self._update_tool(
+            agent_id=agent_id,
+            tool_id=tool_id,
+            definition=definition,
+        )
+
+    @beartype
+    def delete_tool(
+        self,
+        agent_id: Union[str, UUID],
+        tool_id: Union[str, UUID],
+    ):
+        return self._delete_tool(
+            agent_id=agent_id,
+            tool_id=tool_id,
+        )
+
+    @beartype
+    def get_memories(
+        self,
+        agent_id: str,
+        *,
+        query: str,
+        types: Optional[Union[str, List[str]]] = None,
+        user_id: Optional[str] = None,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Memory]:
+        return self._get_memories(
+            agent_id=agent_id,
+            query=query,
+            types=types,
+            user_id=user_id,
+            limit=limit,
+            offset=offset,
+        ).items
+
 
 class AsyncAgentsManager(BaseAgentsManager):
     @beartype
@@ -161,3 +454,153 @@ class AsyncAgentsManager(BaseAgentsManager):
             model,
             docs,
         )
+
+    @beartype
+    async def list(
+        self,
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Agent]:
+        return (
+            await self._list_items(
+                limit=limit,
+                offset=offset,
+            )
+        ).items
+
+    @beartype
+    async def delete(self, agent_id: Union[str, UUID]):
+        return await self._delete(agent_id=agent_id)
+
+    @beartype
+    async def update(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        about: Optional[str] = None,
+        instructions: Optional[Union[List[str], List[InstructionDict]]] = None,
+        name: Optional[str] = None,
+        model: Optional[str] = None,
+        default_settings: Optional[DefaultSettingsDict] = None,
+    ) -> ResourceUpdatedResponse:
+        return await self._update(
+            agent_id=agent_id,
+            about=about,
+            instructions=instructions,
+            name=name,
+            model=model,
+            default_settings=default_settings,
+        )
+
+    @beartype
+    async def get_additional_info(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[AdditionalInfo]:
+        return (
+            await self._get_additional_info(
+                agent_id=agent_id,
+                limit=limit,
+                offset=offset,
+            )
+        ).items
+
+    @beartype
+    async def create_additional_info(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        doc: DocDict,
+    ) -> ResourceCreatedResponse:
+        return await self._create_additional_info(
+            agent_id=agent_id,
+            doc=doc,
+        )
+
+    @beartype
+    async def delete_additional_info(
+        self, agent_id: Union[str, UUID], additional_info_id: Union[str, UUID]
+    ):
+        return await self._delete_additional_info(
+            agent_id=agent_id,
+            additional_info_id=additional_info_id,
+        )
+
+    @beartype
+    async def get_tools(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Tool]:
+        return (
+            await self._get_tools(
+                agent_id=agent_id,
+                limit=limit,
+                offset=offset,
+            )
+        ).items
+
+    @beartype
+    async def create_tool(
+        self,
+        agent_id: Union[str, UUID],
+        *,
+        tool: ToolDict,
+    ) -> ResourceCreatedResponse:
+        return await self._create_tool(
+            agent_id=agent_id,
+            tool=tool,
+        )
+
+    @beartype
+    async def update_tool(
+        self,
+        agent_id: Union[str, UUID],
+        tool_id: Union[str, UUID],
+        *,
+        definition: FunctionDefDict,
+    ) -> ResourceUpdatedResponse:
+        return await self._update_tool(
+            agent_id=agent_id,
+            tool_id=tool_id,
+            definition=definition,
+        )
+
+    @beartype
+    async def delete_tool(
+        self,
+        agent_id: Union[str, UUID],
+        tool_id: Union[str, UUID],
+    ):
+        return await self._delete_tool(
+            agent_id=agent_id,
+            tool_id=tool_id,
+        )
+
+    @beartype
+    async def get_memories(
+        self,
+        agent_id: str,
+        *,
+        query: str,
+        types: Optional[Union[str, List[str]]] = None,
+        user_id: Optional[str] = None,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Memory]:
+        return (
+            await self._get_memories(
+                agent_id=agent_id,
+                query=query,
+                types=types,
+                user_id=user_id,
+                limit=limit,
+                offset=offset,
+            )
+        ).items
--- sdks/python/tests/test_agents.py
+++ sdks/python/tests/test_agents.py
@@ -2,7 +2,13 @@ from uuid import uuid4
 
 from ward import test
 
-from julep_ai.api.types import Agent, ResourceCreatedResponse
+from julep_ai.api.types import (
+    Agent,
+    ResourceCreatedResponse,
+    ResourceUpdatedResponse,
+    AdditionalInfo,
+    Tool,
+)
 
 from .fixtures import async_client, client
 
@@ -43,3 +49,213 @@ async def _(client=async_client):
 
     assert isinstance(response, ResourceCreatedResponse)
     assert response.created_at
+
+
+@test("agents.list")
+def _(client=client):
+    response = client.agents.list()
+    assert len(response) > 0
+    assert isinstance(response[0], Agent)
+
+
+@test("async agents.list")
+async def _(client=async_client):
+    response = await client.agents.list()
+    assert len(response) > 0
+    assert isinstance(response[0], Agent)
+
+
+@test("agents.update")
+def _(client=client):
+    response = client.agents.update(
+        uuid4(),
+        name="test user",
+        about="test user about",
+        instructions=["test agent instructions"],
+        default_settings={"temperature": 0.5},
+        model="some model",
+    )
+
+    assert isinstance(response, ResourceUpdatedResponse)
+    assert response.updated_at
+
+
+@test("agents.get_additional_info")
+def _(client=client):
+    response = client.agents.get_additional_info(uuid4())
+    assert len(response) > 0
+    assert isinstance(response[0], AdditionalInfo)
+
+
+@test("agents.create_additional_info")
+def _(client=client):
+    response = client.agents.create_additional_info(
+        uuid4(), doc={"title": "test title", "content": "test content"}
+    )
+    assert isinstance(response, ResourceCreatedResponse)
+
+
+@test("agents.delete_additional_info")
+def _(client=client):
+    response = client.agents.delete_additional_info(
+        uuid4(),
+        additional_info_id=uuid4(),
+    )
+    assert response is None
+
+
+@test("agents.get_tools")
+def _(client=client):
+    response = client.agents.get_tools(uuid4())
+    assert len(response) > 0
+    assert isinstance(response[0], Tool)
+
+
+@test("agents.create_tool")
+def _(client=client):
+    response = client.agents.create_tool(
+        uuid4(),
+        tool={
+            "type": "function",
+            "definition": {
+                "description": "test description",
+                "name": "test name",
+                "parameters": {"test_arg": "test val"},
+            },
+        },
+    )
+
+    assert isinstance(response, ResourceCreatedResponse)
+    assert response.created_at
+
+
+@test("agents.update_tool")
+def _(client=client):
+    response = client.agents.update_tool(
+        uuid4(),
+        uuid4(),
+        definition={
+            "description": "test description",
+            "name": "test name",
+            "parameters": {"test_arg": "test val"},
+        },
+    )
+
+    assert isinstance(response, ResourceUpdatedResponse)
+    assert response.updated_at
+
+
+@test("agents.delete_tool")
+def _(client=client):
+    response = client.agents.delete_tool(
+        uuid4(),
+        uuid4(),
+    )
+    assert response is None
+
+
+@test("async agents.update")
+async def _(client=async_client):
+    response = await client.agents.update(
+        uuid4(),
+        name="test user",
+        about="test user about",
+        instructions=["test agent instructions"],
+        default_settings={"temperature": 0.5},
+        model="some model",
+    )
+
+    assert isinstance(response, ResourceUpdatedResponse)
+    assert response.updated_at
+
+
+@test("async agents.get_additional_info")
+async def _(client=async_client):
+    response = await client.agents.get_additional_info(uuid4())
+    assert len(response) > 0
+    assert isinstance(response[0], AdditionalInfo)
+
+
+@test("async agents.create_additional_info")
+async def _(client=async_client):
+    response = await client.agents.create_additional_info(
+        uuid4(), doc={"title": "test title", "content": "test content"}
+    )
+    assert isinstance(response, ResourceCreatedResponse)
+
+
+@test("async agents.delete_additional_info")
+async def _(client=async_client):
+    response = await client.agents.delete_additional_info(
+        uuid4(),
+        additional_info_id=uuid4(),
+    )
+    assert response is None
+
+
+@test("async agents.get_tools")
+async def _(client=async_client):
+    response = await client.agents.get_tools(uuid4())
+    assert len(response) > 0
+    assert isinstance(response[0], Tool)
+
+
+@test("agents.delete")
+def _(client=client):
+    response = client.agents.delete(
+        uuid4(),
+    )
+
+    assert response is None
+
+
+@test("async agents.delete")
+async def _(client=async_client):
+    response = await client.agents.delete(
+        uuid4(),
+    )
+
+    assert response is None
+
+
+@test("agents.create_tool")
+async def _(client=async_client):
+    response = await client.agents.create_tool(
+        uuid4(),
+        tool={
+            "type": "function",
+            "definition": {
+                "description": "test description",
+                "name": "test name",
+                "parameters": {"test_arg": "test val"},
+            },
+        },
+    )
+
+    assert isinstance(response, ResourceCreatedResponse)
+    assert response.created_at
+
+
+@test("async agents.update_tool")
+async def _(client=async_client):
+    response = await client.agents.update_tool(
+        uuid4(),
+        uuid4(),
+        definition={
+            "description": "test description",
+            "name": "test name",
+            "parameters": {"test_arg": "test val"},
+        },
+    )
+
+    assert isinstance(response, ResourceUpdatedResponse)
+    assert response.updated_at
+
+
+@test("async agents.delete_tool")
+async def _(client=async_client):
+    response = await client.agents.delete_tool(
+        uuid4(),
+        uuid4(),
+    )
+    assert response is None
