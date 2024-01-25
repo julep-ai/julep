6d428085d6706548a5cc169584c5cc995ef12137
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-01-25 06:05:48 +0530

    feat: Rename python package julep_ai to julep
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- sdks/python/tests/test_users.py
+++ sdks/python/tests/test_users.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 
 from ward import test
 
-from julep_ai.api.types import User, ResourceCreatedResponse, ResourceUpdatedResponse
+from julep.api.types import User, ResourceCreatedResponse, ResourceUpdatedResponse
 
 from .fixtures import async_client, client
 
