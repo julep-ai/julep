5e5dad4731b311f2542c5f58f75c6da915119056
Dmitry Paramonov <asmatic075@gmail.com>
2024-01-26 14:13:03 +0300

    feat: Add entries summarization workflow

---
+++ memory/memory_api/workflows/summarization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+from uuid import UUID
+from datetime import timedelta
+from temporalio import workflow
+
+with workflow.unsafe.imports_passed_through():
+    from ..activities.summarization import summarization
+
+
+@workflow.defn
+class SummarizationWorkflow:
+    @workflow.run
+    async def run(self, session_id: UUID) -> None:
+        return await workflow.execute_activity(
+            summarization,
+            session_id,
+            schedule_to_close_timeout=timedelta(seconds=5),
+        )
