20a90f99d6e4c66e75af115d003f131eb5be65d3
Dmitry Paramonov <asmatic075@gmail.com>
2024-02-07 16:04:45 +0300

    fix: Generate JSON output only if the corresponding response format is set

--- samantha-api/samantha_api/utils.py
+++ samantha-api/samantha_api/utils.py
@@ -1,6 +1,6 @@
 from lmformatenforcer import CharacterLevelParser
 from lmformatenforcer.integrations.vllm import (
-    build_vllm_logits_processor, 
+    build_vllm_logits_processor,
 )
 from lmformatenforcer.integrations.transformers import (
     build_token_enforcer_tokenizer_data,
@@ -64,7 +64,7 @@ def validate_functions(functions: list[dict]):
 
 def build_vllm_token_enforcer_tokenizer_data(tokenizer) -> TokenEnforcerTokenizerData:
     # In some vLLM versions the tokenizer is wrapped in a TokenizerGroup
-    if tokenizer.__class__.__name__ == 'TokenizerGroup':
+    if tokenizer.__class__.__name__ == "TokenizerGroup":
         tokenizer = tokenizer.tokenizer  # noqa
     return build_token_enforcer_tokenizer_data(tokenizer)
 
@@ -78,7 +78,7 @@ def vllm_with_character_level_parser(
     parser: Optional[CharacterLevelParser] = None,
 ) -> ListOrStrList:
     tokenizer_data = build_vllm_token_enforcer_tokenizer_data(tokenizer)
-    
+
     if parser:
         logits_processor = build_vllm_logits_processor(tokenizer_data, parser)
         sampling_params.logits_processors = [logits_processor]
