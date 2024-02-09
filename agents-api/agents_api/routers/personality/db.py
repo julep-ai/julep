5b4dea7d55394c23d685e77dc7e1d61aa9e4ee49
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-09 21:13:05 +0530

    refactor: Rename package memory-api to agents-api
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ agents-api/agents_api/routers/personality/db.py
@@ -0,0 +1,133 @@
+import logging
+from agents_api.clients.cozo import client
+from pydantic import UUID4
+from pandas import DataFrame
+
+
+logger = logging.getLogger(__name__)
+
+
+def create_account_query(user_id: UUID4, account_id: str) -> DataFrame:
+    query = f"""
+    ?[
+        user_id,
+        account_id,
+    ] <- [[
+    to_uuid("{user_id}"),
+    "{account_id}",
+    ]]
+
+    :put user_principles {{
+        user_id,
+        account_id,
+    }}
+    """
+
+    return client.run(query)
+
+
+def get_account_query(user_id: UUID4) -> DataFrame:
+    query = f"""
+    input[user_id] <- [[
+        to_uuid("{user_id}"),
+    ]]
+
+    ?[
+        user_id,
+        account_id,
+        full,
+        processed_at,
+        scores,
+    ] := input[user_id],
+    *user_principles {{
+        user_id,
+        account_id,
+        full,
+        processed_at,
+        scores,
+        @ "NOW"
+    }}
+    """
+
+    return client.run(query)
+
+
+def save_results_query(
+    user_id: UUID4, scores: dict, full_assesment: bool = False
+) -> DataFrame:
+    query = f"""
+    input[
+        user_id,
+        full,
+        scores,
+    ] <- [[
+        to_uuid("{user_id}"),
+        {str(full_assesment).lower()},  # true if full assessment results
+        {scores},  # json returned as result
+    ]]
+
+    ?[
+        user_id,
+        full,
+        scores,
+        account_id,
+        processed_at,
+    ] := input[
+        user_id,
+        full,
+        scores,
+    ],
+        *user_principles {{
+        user_id,
+        account_id,
+    }}, processed_at = null
+
+    :put user_principles {{
+        user_id,
+        account_id,
+        full,
+        scores,
+        processed_at,
+    }}
+    """
+
+    return client.run(query)
+
+
+def get_user_data(user_id: UUID4) -> DataFrame:
+    query = f"""
+    input[user_id] <- [[
+        to_uuid("{user_id}"),
+    ]]
+
+    ?[
+        user_id,
+        name,
+        email,
+    ] := input[user_id],
+    *users {{
+        user_id,
+        name,
+        email,
+    }}
+    """
+
+    return client.run(query)
+
+
+def init():
+    query = """
+    :create user_principles {
+        user_id: Uuid,
+        account_id: String,
+        full: Bool default false,
+        updated_at: Validity default [floor(now()), true],
+        =>
+        processed_at: Float? default null,
+        scores: Json? default null
+    }
+    """
+    try:
+        client.run(query)
+    except Exception as e:
+        logger.exception(e)
---
+++ agents-api/agents_api/routers/sessions/protocol.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from agents_api.autogen.openapi_model import ResponseFormat
+
+
+class Settings(BaseModel):
+    model: str
+    frequency_penalty: float | None = Field(default=0)
+    length_penalty: float | None = Field(default=1.0)
+    logit_bias: float | None = None
+    max_tokens: int
+    presence_penalty: float | None = Field(default=0)
+    repetition_penalty: float | None = Field(default=1)
+    response_format: ResponseFormat | None
+    seed: int | None = Field(default=0)
+    stop: list[str] | None = None
+    stream: bool = Field(default=False)
+    temperature: float | None = Field(default=0.7)
+    top_p: float | None = Field(default=1)
+    remember: bool | None = Field(default=True)
+    recall: bool | None = Field(default=True)
