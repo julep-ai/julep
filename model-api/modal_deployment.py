9575477a4f7c70831cbd5125e064acd3a1d8b54e
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-09 16:13:39 +0530

    fix: Fix modal deployment
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- model-api/modal_deployment.py
+++ model-api/modal_deployment.py
@@ -2,6 +2,8 @@ import os
 
 from modal import Image, Secret, Stub, asgi_app, gpu
 
+import model_api  # noqa: F401
+
 ## Needed so that modal knows to package this directory
 
 MODEL_DIR = "/model"
@@ -29,8 +31,9 @@ image = (
         "vllm==0.3.0",
         "huggingface_hub==0.20.3",
         "hf-transfer==0.1.5",
+        "uvicorn==0.27.0.post1",
         "torch==2.1.2",
-        "prometheus-client==0.19.0",
+        "aioprometheus==23.12.0",
         "psutil==5.9.8",
         "starlette-exporter==0.17.1",
         "environs==10.3.0",
@@ -38,6 +41,8 @@ image = (
         "fastapi==0.109.2",
         "sentry-sdk==1.40.1",
         "jsonschema==4.21.1",
+        "lm-format-enforcer==0.8.3",
+        "interegular==0.3.3",
     )
     # Use the barebones hf-transfer package for maximum download speeds. No progress bar, but expect 700MB/s.
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
@@ -70,12 +75,11 @@ def get_app():
 
     assert os.environ["API_KEY"]
     REVISION = os.environ["REVISION"]
-    BACKLOG = os.environ["BACKLOG"]
     BLOCK_SIZE = os.environ["BLOCK_SIZE"]
     MAX_MODEL_LEN = os.environ["MAX_MODEL_LEN"]
     MODEL_NAME = os.environ["MODEL_NAME"]
 
-    app, args = create_app(
+    app = create_app(
         [
             "--model",
             MODEL_NAME,
@@ -83,8 +87,6 @@ def get_app():
             MODEL_NAME,
             "--revision",
             REVISION,
-            "--backlog",
-            BACKLOG,
             "--block-size",
             BLOCK_SIZE,
             "--max-model-len",
--- model-api/model_api/env.py
+++ model-api/model_api/env.py
@@ -7,3 +7,6 @@ env.read_env()
 
 sentry_dsn: str = env.str("SENTRY_DSN", default="")
 api_key: str = env.str("API_KEY")
+host: str = env.str("HOST", default="0.0.0.0")
+port: int = env.int("PORT", default=8000)
+backlog: int = env.int("BACKLOG", default=2048)
