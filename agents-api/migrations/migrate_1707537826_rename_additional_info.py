21be38cea86df7fe51eedc3a9dc2b380de5e9bf9
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-10 10:44:22 +0530

    feat: Migration to rename additional_info to doc
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ agents-api/migrations/migrate_1707537826_rename_additional_info.py
@@ -0,0 +1,167 @@
+# /usr/bin/env python3
+
+MIGRATION_ID = "rename_additional_info"
+CREATED_AT = 1707537826.539182
+
+rename_agent_additional_info_id = dict(
+    up="""
+    ?[agent_id, doc_id, created_at] :=
+        *agent_additional_info{
+            agent_id,
+            additional_info_id: doc_id,
+            created_at,
+        }
+
+    :replace agent_additional_info {
+        agent_id: Uuid,
+        doc_id: Uuid
+        =>
+        created_at: Float default now(),
+    }
+    """,
+    down="""
+    ?[agent_id, additional_info_id, created_at] :=
+        *agent_additional_info{
+            agent_id,
+            doc_id: additional_info_id,
+            created_at,
+        }
+
+    :replace agent_additional_info {
+        agent_id: Uuid,
+        additional_info_id: Uuid
+        =>
+        created_at: Float default now(),
+    }
+    """,
+)
+
+
+rename_user_additional_info_id = dict(
+    up="""
+    ?[user_id, doc_id, created_at] :=
+        *user_additional_info{
+            user_id,
+            additional_info_id: doc_id,
+            created_at,
+        }
+
+    :replace user_additional_info {
+        user_id: Uuid,
+        doc_id: Uuid
+        =>
+        created_at: Float default now(),
+    }
+    """,
+    down="""
+    ?[user_id, additional_info_id, created_at] :=
+        *user_additional_info{
+            user_id,
+            doc_id: additional_info_id,
+            created_at,
+        }
+
+    :replace user_additional_info {
+        user_id: Uuid,
+        additional_info_id: Uuid
+        =>
+        created_at: Float default now(),
+    }
+    """,
+)
+
+
+rename_information_snippets_additional_info_id = dict(
+    up="""
+    ?[
+        doc_id,
+        snippet_idx,
+        title,
+        snippet,
+        embed_instruction,
+        embedding,
+    ] :=
+        *information_snippets{
+            snippet_idx,
+            title,
+            snippet,
+            embed_instruction,
+            embedding,
+            additional_info_id: doc_id,
+        }
+
+    :replace information_snippets {
+        doc_id: Uuid,
+        snippet_idx: Int,
+        =>
+        title: String,
+        snippet: String,
+        embed_instruction: String default 'Encode this passage for retrieval: ',
+        embedding: <F32; 768>? default null,
+    }
+    """,
+    down="""
+    ?[
+        additional_info_id,
+        snippet_idx,
+        title,
+        snippet,
+        embed_instruction,
+        embedding,
+    ] :=
+        *information_snippets{
+            snippet_idx,
+            title,
+            snippet,
+            embed_instruction,
+            embedding,
+            doc_id: additional_info_id,
+        }
+
+    :replace information_snippets {
+        additional_info_id: Uuid,
+        snippet_idx: Int,
+        =>
+        title: String,
+        snippet: String,
+        embed_instruction: String default 'Encode this passage for retrieval: ',
+        embedding: <F32; 768>? default null,
+    }
+    """,
+)
+
+rename_relations = dict(
+    up="""
+    ::rename
+      agent_additional_info -> agent_docs,
+      user_additional_info -> user_docs
+    """,
+    down="""
+    ::rename
+      agent_docs -> agent_additional_info,
+      user_docs -> user_additional_info
+    """,
+)
+
+
+queries_to_run = [
+    rename_agent_additional_info_id,
+    rename_user_additional_info_id,
+    rename_relations,
+]
+
+
+def run(client, *queries):
+    joiner = "}\n\n{"
+
+    query = joiner.join(queries)
+    query = f"{{\n{query}\n}}"
+    client.run(query)
+
+
+def up(client):
+    run(client, *[q["up"] for q in queries_to_run])
+
+
+def down(client):
+    run(client, *[q["down"] for q in reversed(queries_to_run)])
