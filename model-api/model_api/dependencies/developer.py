0cb639557d6614634041fd8be95d3d42d1039c89
Dmitry Paramonov <asmatic075@gmail.com>
2024-02-14 09:13:48 +0300

    fix: Fix headers checking logic

--- model-api/model_api/dependencies/developer.py
+++ model-api/model_api/dependencies/developer.py
@@ -8,17 +8,17 @@ from .exceptions import InvalidHeaderFormat
 
 
 async def get_developer_id(x_developer_id: Annotated[str | None, Header()] = None):
-    # if x_developer_id is None:
-    #     if not skip_check_developer_headers:
-    #         raise InvalidHeaderFormat("X-Developer-Id header invalid")
-    #     else:
-    #         x_developer_id = uuid.UUID("00000000-0000-0000-0000-000000000000")
+    if not x_developer_id:
+        if not skip_check_developer_headers:
+            raise InvalidHeaderFormat("X-Developer-Id header invalid")
+        else:
+            x_developer_id = uuid.UUID("00000000-0000-0000-0000-000000000000")
 
-    # if isinstance(x_developer_id, str):
-    #     try:
-    #         x_developer_id = uuid.UUID(x_developer_id, version=4)
-    #     except ValueError:
-    #         raise InvalidHeaderFormat("X-Developer-Id must be a valid UUID")
+    if isinstance(x_developer_id, str):
+        try:
+            x_developer_id = uuid.UUID(x_developer_id, version=4)
+        except ValueError:
+            raise InvalidHeaderFormat("X-Developer-Id must be a valid UUID")
 
     return x_developer_id or uuid.UUID("00000000-0000-0000-0000-000000000000")
 
@@ -26,15 +26,15 @@ async def get_developer_id(x_developer_id: Annotated[str | None, Header()] = Non
 async def get_developer_email(
     x_developer_email: Annotated[str | None, Header()] = None
 ):
-    # if x_developer_email is None:
-    #     if not skip_check_developer_headers:
-    #         raise InvalidHeaderFormat("X-Developer-Email header invalid")
-    #     else:
-    #         x_developer_email = "unknown_user@mail.com"
+    if not x_developer_email:
+        if not skip_check_developer_headers:
+            raise InvalidHeaderFormat("X-Developer-Email header invalid")
+        else:
+            x_developer_email = "unknown_user@mail.com"
 
-    # try:
-    #     validate_email(x_developer_email)
-    # except PydanticCustomError:
-    #     raise InvalidHeaderFormat("X-Developer-Email header invalid")
+    try:
+        validate_email(x_developer_email)
+    except PydanticCustomError:
+        raise InvalidHeaderFormat("X-Developer-Email header invalid")
 
     return x_developer_email or "unknown_user@mail.com"
