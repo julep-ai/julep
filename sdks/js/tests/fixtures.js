f15b5564160296c0de712f12664d5eddf41b2dcd
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-26 17:17:35 +0530

    feat: Regenerate fern clients
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- sdks/js/tests/fixtures.js
+++ sdks/js/tests/fixtures.js
@@ -4,7 +4,7 @@ const { Client } = require("../");
 
 function setupClient() {
   // Mock server base URL
-  const baseUrl = "http://localhost:8080";
+  const baseUrl = "http://localhost:8080/api";
   const apiKey = "thisisnotarealapikey";
   const client = new Client({ apiKey, baseUrl });
 
