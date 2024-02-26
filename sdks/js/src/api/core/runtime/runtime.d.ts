141dbf3e01d6980e9b506b9d58c499066108edc1
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-26 17:17:35 +0530

    feat: Regenerate fern clients
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ sdks/js/src/api/core/runtime/runtime.d.ts
@@ -0,0 +1,15 @@
+/**
+ * A constant that indicates which environment and version the SDK is running in.
+ */
+export declare const RUNTIME: Runtime;
+export interface Runtime {
+  type:
+    | "browser"
+    | "web-worker"
+    | "deno"
+    | "bun"
+    | "node"
+    | "react-native"
+    | "unknown";
+  version?: string;
+}
