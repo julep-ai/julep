04074447c209b77331f2854632bd661db66618ce
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-26 23:11:42 +0530

    feat: Trim trailing space in chat template
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- model-api/test_chat_template.py
+++ model-api/test_chat_template.py
@@ -4,17 +4,26 @@ model_name = "julep-ai/samantha-1-turbo"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
-def to_prompt(messages, **kwargs):
-    prompt = tokenizer.apply_chat_template(messages, tokenize=False, **kwargs)
+# Set the template
+template_path = "./model_api/chat_template.jinja"
+with open(template_path, "r") as f:
+    chat_template = f.read()
+
+
+def to_prompt(messages, chat_template=chat_template, **kwargs):
+    prompt = tokenizer.apply_chat_template(
+        messages, chat_template=chat_template, tokenize=False, **kwargs
+    )
+
     return prompt
 
 
 def test_function_call_none_last_user_continue():
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
-        ({"role": "assistant", "name": "Samantha", "continue": True}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
+        {"role": "assistant", "name": "Samantha", "continue": True},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -31,11 +40,12 @@ Hey!<|im_end|>
 """
     )
 
+
 def test_function_call_none_last_not_continue():
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -50,14 +60,13 @@ Hey John<|im_end|>
 Hey!<|im_end|>
 <|im_start|>"""
     )
-
 
 
 def test_function_call_auto_functions_not_passed():
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -74,12 +83,11 @@ Hey!<|im_end|>
     )
 
 
-
 def test_function_call_none_functions_not_passed():
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -98,28 +106,27 @@ Hey!<|im_end|>
 
 def test_function_call_auto_functions_passed():
     functions = [
-        ({
-                "name": "generate_anagram",
-                "description": "Generate an anagram of a given word",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "word": {
-                            "type": "string",
-                            "description": "The word to generate an anagram of",
-                        }
-                    },
-                    "required": ["word"],
+        {
+            "name": "generate_anagram",
+            "description": "Generate an anagram of a given word",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "word": {
+                        "type": "string",
+                        "description": "The word to generate an anagram of",
+                    }
                 },
-            }
-        )
+                "required": ["word"],
+            },
+        }
     ]
 
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "system", "name": "functions", "content": functions}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "system", "name": "functions", "content": functions},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -155,73 +162,72 @@ Hey!<|im_end|>
     )
 
 
-def test_function_call_none_functions_passed():
-    messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
-    ]
-    functions = [
-        ({
-                "name": "generate_anagram",
-                "description": "Generate an anagram of a given word",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "word": {
-                            "type": "string",
-                            "description": "The word to generate an anagram of",
-                        }
-                    },
-                    "required": ["word"],
-                },
-            }
-        )
-    ]
-    prompt = to_prompt(
-        messages,
-        bos="<|im_start|>",
-        eos="<|im_end|>",
-        functions=functions,
-        function_call=None,
-    )
-    assert (
-        prompt
-        == """<|im_start|>situation
-I am talking to John<|im_end|>
-<|im_start|>functions
-Available functions:
+# def test_function_call_none_functions_passed():
+#     messages = [
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
+#     ]
+#     functions = [
+#         {
+#             "name": "generate_anagram",
+#             "description": "Generate an anagram of a given word",
+#             "parameters": {
+#                 "type": "object",
+#                 "properties": {
+#                     "word": {
+#                         "type": "string",
+#                         "description": "The word to generate an anagram of",
+#                     }
+#                 },
+#                 "required": ["word"],
+#             },
+#         }
+#     ]
+#     prompt = to_prompt(
+#         messages,
+#         bos="<|im_start|>",
+#         eos="<|im_end|>",
+#         functions=functions,
+#         function_call=None,
+#     )
+#     assert (
+#         prompt
+#         == """<|im_start|>situation
+# I am talking to John<|im_end|>
+# <|im_start|>functions
+# Available functions:
 
-{
-    "name": "generate_anagram",
-    "description": "Generate an anagram of a given word",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "word": {
-                "type": "string",
-                "description": "The word to generate an anagram of"
-            }
-        },
-        "required": [
-            "word"
-        ]
-    }
-}<|im_end|>
-<|im_start|>me (Samantha)
-Hey John<|im_end|>
-<|im_start|>person (John)
-Hey!<|im_end|>
-<|im_start|>"""
-    )
+# {
+#     "name": "generate_anagram",
+#     "description": "Generate an anagram of a given word",
+#     "parameters": {
+#         "type": "object",
+#         "properties": {
+#             "word": {
+#                 "type": "string",
+#                 "description": "The word to generate an anagram of"
+#             }
+#         },
+#         "required": [
+#             "word"
+#         ]
+#     }
+# }<|im_end|>
+# <|im_start|>me (Samantha)
+# Hey John<|im_end|>
+# <|im_start|>person (John)
+# Hey!<|im_end|>
+# <|im_start|>"""
+#     )
 
 
 def test_function_call_none_last_continue():
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hi", "continue": True}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
+        {"role": "assistant", "name": "Samantha", "content": "Hi", "continue": True},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -241,10 +247,10 @@ Hi"""
 
 def test_function_call_none_last_continue_function_call():
     messages = [
-        ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-        ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-        ({"role": "user", "name": "John", "content": "Hey!"}),
-        ({"role": "function_call", "content": "{}", "continue": True}),
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
+        {"role": "function_call", "content": "{}", "continue": True},
     ]
 
     prompt = to_prompt(messages, add_generation_prompt=True)
@@ -264,9 +270,9 @@ Hey!<|im_end|>
 
 # def test_function_call_auto_last_not_continue():
 #     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
 #     ]
 #     functions = [
 #         ({
@@ -352,14 +358,14 @@ Hey!<|im_end|>
 # Hey!<|im_end|>
 # <|im_start|>"""
 #     )
-#
-#
+
+
 # def test_function_call_auto_last_continue():
 #     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
-#         ({"role": "assistant", "name": "Samantha", "continue": True}),
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
+#         {"role": "assistant", "name": "Samantha", "continue": True},
 #     ]
 #     functions = [
 #         ({
@@ -415,14 +421,14 @@ Hey!<|im_end|>
 # <|im_start|>me (Samantha)
 # """
 #     )
-#
-#
+
+
 # def test_function_call_auto_last_continue_function_call():
 #     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
-#         ({"role": "function_call", "continue": True}),
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
+#         {"role": "function_call", "continue": True},
 #     ]
 #     functions = [
 #         ({
@@ -478,13 +484,13 @@ Hey!<|im_end|>
 # <|im_start|>function_call
 # """
 #     )
-#
-#
+
+
 # def test_function_call_func_name_last_not_continue():
 #     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
 #     ]
 #     functions = [
 #         ({
@@ -523,7 +529,7 @@ Hey!<|im_end|>
 #         bos="<|im_start|>",
 #         eos="<|im_end|>",
 #         functions=functions,
-#         function_call=FunctionCall(**{"name": "generate_anagram"}),
+#         function_call=FunctionCall(**{"name": "generate_anagram"},
 #     )
 #     assert (
 #         prompt
@@ -555,13 +561,13 @@ Hey!<|im_end|>
 # <|im_start|>function_call
 # {"name": "generate_anagram","""
 #     )
-#
-#
+
+
 # def test_function_call_func_name_last_not_continue_invalid_function_name():
 #     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
 #     ]
 #     functions = [
 #         ({
@@ -601,17 +607,17 @@ Hey!<|im_end|>
 #             bos="<|im_start|>",
 #             eos="<|im_end|>",
 #             functions=functions,
-#             function_call=FunctionCall(**{"name": "unknown"}),
+#             function_call=FunctionCall(**{"name": "unknown"},
 #         )
 #     assert e_info.value.args[0] == "Invalid function name: unknown"
-#
-#
+
+
 # def test_function_call_func_name_last_continue():
 #     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
-#         ({"role": "assistant", "name": "Samantha", "continue": True}),
+#         {"role": "system", "name": "situation", "content": "I am talking to John"},
+#         {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+#         {"role": "user", "name": "John", "content": "Hey!"},
+#         {"role": "assistant", "name": "Samantha", "continue": True},
 #     ]
 #     functions = [
 #         ({
@@ -636,7 +642,7 @@ Hey!<|im_end|>
 #             bos="<|im_start|>",
 #             eos="<|im_end|>",
 #             functions=functions,
-#             function_call=FunctionCall(**{"name": "generate_anagram"}),
+#             function_call=FunctionCall(**{"name": "generate_anagram"},
 #         )
 #     assert e_info.value.args[0] == (
 #         "Invalid prompt format: Conflicting instructions, "
@@ -644,52 +650,71 @@ Hey!<|im_end|>
 #         "flag set to 'true' or set the flag to 'false'. "
 #         "You can either remove `functions` and/or `function_call` parameters."
 #     )
-#
-#
-# def test_function_call_func_name_last_continue_function_call():
-#     messages = [
-#         ({"role": "system", "name": "situation", "content": "I am talking to John"}),
-#         ({"role": "assistant", "name": "Samantha", "content": "Hey John"}),
-#         ({"role": "user", "name": "John", "content": "Hey!"}),
-#         (
-#             {
-#                 "role": "function_call",
-#                 "content": '{"name": "generate_anagram", ',
-#                 "continue": True,
-#             }
-#         ),
-#     ]
-#     functions = [
-#         ({
-#                 "name": "generate_anagram",
-#                 "description": "Generate an anagram of a given word",
-#                 "parameters": {
-#                     "type": "object",
-#                     "properties": {
-#                         "word": {
-#                             "type": "string",
-#                             "description": "The word to generate an anagram of",
-#                         }
-#                     },
-#                     "required": ["word"],
-#                 },
-#             }
-#         )
-#     ]
-#     with pytest.raises(InvalidPromptException) as e_info:
-#         to_prompt(
-#             messages,
-#             bos="<|im_start|>",
-#             eos="<|im_end|>",
-#             functions=functions,
-#             function_call=FunctionCall(**{"name": "generate_anagram"}),
-#         )
-#     assert e_info.value.args[0] == (
-#         "Invalid prompt format: Conflicting instructions, "
-#         "please remove the last instruction with 'continue' "
-#         "flag set to 'true' or set the flag to 'false'. "
-#         "You can either remove `functions` and/or `function_call` parameters."
-#     )
+
+
+def test_function_call_func_name_last_continue_function_call():
+    functions = [
+        {
+            "name": "generate_anagram",
+            "description": "Generate an anagram of a given word",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "word": {
+                        "type": "string",
+                        "description": "The word to generate an anagram of",
+                    }
+                },
+                "required": ["word"],
+            },
+        }
+    ]
+
+    messages = [
+        {"role": "system", "name": "situation", "content": "I am talking to John"},
+        {"role": "system", "name": "functions", "content": functions},
+        {"role": "assistant", "name": "Samantha", "content": "Hey John"},
+        {"role": "user", "name": "John", "content": "Hey!"},
+        {
+            "role": "function_call",
+            "content": '{"name": "generate_anagram", ',
+            "continue": True,
+        },
+    ]
+
+    prompt = to_prompt(messages, add_generation_prompt=True)
+
+    expected = """\
+<s><|im_start|>situation
+I am talking to John<|im_end|>
+<|im_start|>functions
+Available functions:
+
+{
+    "description": "Generate an anagram of a given word",
+    "name": "generate_anagram",
+    "parameters": {
+        "properties": {
+            "word": {
+                "description": "The word to generate an anagram of",
+                "type": "string"
+            }
+        },
+        "required": [
+            "word"
+        ],
+        "type": "object"
+    }
+}<|im_end|>
+<|im_start|>me (Samantha)
+Hey John<|im_end|>
+<|im_start|>person (John)
+Hey!<|im_end|>
+<|im_start|>function_call
+{"name": "generate_anagram","""
+
+    assert prompt == expected
+
 
 if __name__ == "__main__":
     test_function_call_none_last_user_continue()
@@ -699,3 +724,4 @@ if __name__ == "__main__":
     test_function_call_none_last_continue()
     test_function_call_none_last_continue_function_call()
     test_function_call_auto_functions_passed()
+    test_function_call_func_name_last_continue_function_call()
