2e3a09df62b65ffda61472e04256578a5baa9887
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-02-28 15:01:33 +0530

    fix: Parameterize fixture
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- sdks/python/tests/fixtures.py
+++ sdks/python/tests/fixtures.py
@@ -1,20 +1,31 @@
+from typing import Optional
+
+from environs import Env
 from ward import fixture
+
 from julep import AsyncClient, Client
+
+env = Env()
+
+TEST_API_KEY: Optional[str] = env.str("TEST_API_KEY", "thisisnotarealapikey")
+TEST_API_URL: Optional[str] = env.str("TEST_API_URL", "http://localhost:8080/api")
 
 
 @fixture(scope="global")
 def client():
-    # Mock server base url
-    base_url = "http://localhost:8080/api"
-    client = Client(api_key="thisisnotarealapikey", base_url=base_url)
+    client = Client(
+        api_key=TEST_API_KEY,
+        base_url=TEST_API_URL,
+    )
 
     return client
 
 
 @fixture
 def async_client():
-    # Mock server base url
-    base_url = "http://localhost:8080/api"
-    client = AsyncClient(api_key="thisisnotarealapikey", base_url=base_url)
+    client = AsyncClient(
+        api_key=TEST_API_KEY,
+        base_url=TEST_API_URL,
+    )
 
     return client
