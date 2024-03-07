ae90fc98659975481eccaeb07c80dfae1f6133ab
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-03-07 23:00:25 +0530

    fix: Fix julep python sdk (url misconfigured)
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- sdks/python/julep/env.py
+++ sdks/python/julep/env.py
@@ -2,7 +2,11 @@ from typing import Optional
 
 from environs import Env
 
+from .api.environment import JulepApiEnvironment
+
 env = Env()
 
 JULEP_API_KEY: Optional[str] = env.str("JULEP_API_KEY", None)
-JULEP_API_URL: Optional[str] = env.str("JULEP_API_URL", "https://api-alpha.julep.ai")
+JULEP_API_URL: Optional[str] = env.str(
+    "JULEP_API_URL", JulepApiEnvironment.DEFAULT.value
+)
