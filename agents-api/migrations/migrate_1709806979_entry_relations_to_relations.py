3b27055d093adef48e97dd678e6e7c056e0e3809
Dmitry Paramonov <asmatic075@gmail.com>
2024-03-07 13:41:14 +0300

    chore: Rename entry_relations to relations

---
+++ agents-api/migrations/migrate_1709806979_entry_relations_to_relations.py
@@ -0,0 +1,30 @@
+#/usr/bin/env python3
+
+MIGRATION_ID = "entry_relations_to_relations"
+CREATED_AT = 1709806979.250619
+
+
+entry_relations_to_relations = {
+    "up": """
+    ::rename
+      entry_relations -> relations,
+    """,
+    "down": """
+    ::rename
+      relations -> entry_relations,
+    """,
+}
+
+queries_to_run = [
+    entry_relations_to_relations,
+]
+
+
+def up(client):
+    for q in queries_to_run:
+        client.run(q["up"])
+
+
+def down(client):
+    for q in queries_to_run:
+        client.run(q["down"])
