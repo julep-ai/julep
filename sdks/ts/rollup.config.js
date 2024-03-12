70973accc661588f8d8730ca3667d39b0a4eaefc
Philip <philip.balbas@gmail.com>
2024-03-12 19:11:09 +0800

    feat: build sdk for browser

---
+++ sdks/ts/rollup.config.js
@@ -0,0 +1,16 @@
+const typescript = require("rollup-plugin-typescript2");
+const pkg = require("./package.json");
+
+module.exports = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [typescript()],
+};
