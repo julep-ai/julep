e5e3a3e499701163d9648e178fa17068a7c0ac45
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-03-12 17:30:36 +0530

    feat: Multiple targets
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- sdks/ts/rollup.config.js
+++ sdks/ts/rollup.config.js
@@ -1,16 +1,35 @@
 const typescript = require("rollup-plugin-typescript2");
 const pkg = require("./package.json");
+const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const json = require("@rollup/plugin-json");
+const strip = require("@rollup/plugin-strip");
+const { optimizeLodashImports } = require("@optimize-lodash/rollup-plugin");
 
 module.exports = {
   input: "src/index.ts",
   output: [
     {
-      file: pkg.main,
+      dir: pkg.main.replace("/index.js", ""),
       format: "cjs",
       exports: "named",
       sourcemap: true,
       strict: false,
     },
+    {
+      dir: pkg.module.replace("/index.js", ""),
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
   ],
-  plugins: [typescript()],
+  plugins: [
+    typescript(),
+    nodeResolve({ exportConditions: ["node"], inlineDynamicImports: true }),
+    commonjs(),
+    optimizeLodashImports(),
+    strip(),
+    json(),
+  ],
 };
