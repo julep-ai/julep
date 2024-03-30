aca1b41eb4c76c7db2d18e70f964e72100d6e3c4
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-03-30 14:02:34 +0530

    feat: Minor refactors
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- traefik-cloud-run/entrypoint.sh
+++ traefik-cloud-run/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-SHARED_KEY="${SHARED_KEY:-empty}" \
-AGENTS_API_KEY="${AGENTS_API_KEY:-empty}" \
-MODEL_API_KEY="${MODEL_API_KEY:-empty}" \
-API_KEY="${API_KEY:-empty}" \
-MODEL_API_URL="${MODEL_API_URL:-empty}" \
-AGENTS_API_URL="${AGENTS_API_URL:-empty}" \
+# Check the environment variables
+for var_name in SHARED_KEY AGENTS_API_KEY MODEL_API_KEY MODEL_API_URL AGENTS_API_URL
+do
+    if [ -z "`eval echo \\\$$var_name`" ]; then
+        echo "Error: Environment variable '$var_name' is not set."
+        exit 1
+    fi
+done
+
 envsubst < /etc/traefik/traefik.yml.template > /etc/traefik/traefik.yml
 exec traefik
