9af99cf2955b4d6a38cbb9f5183fd2c80070f0cf
Dmitry Paramonov <asmatic075@gmail.com>
2024-04-16 10:14:52 +0300

    fix: Set empty string as a default value for function description

--- agents-api/agents_api/routers/agents/routers.py
+++ agents-api/agents_api/routers/agents/routers.py
@@ -487,7 +487,7 @@ async def update_tool(
     embeddings = await embed(
         [
             function_embed_instruction
-            + request.function.description
+            + (request.function.description or "")
             + "\nParameters: "
             + json.dumps(request.function.parameters.model_dump())
         ],
@@ -528,7 +528,7 @@ async def patch_tool(
     embeddings = await embed(
         [
             function_embed_instruction
-            + request.function.description
+            + (request.function.description or "")
             + "\nParameters: "
             + json.dumps(request.function.parameters.model_dump())
         ],
