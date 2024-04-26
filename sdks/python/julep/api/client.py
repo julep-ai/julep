f8d9ae8c36381d5ba48bf0fa66efe26aba1b47b5
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-04-26 11:30:03 +0530

    fix: Minor fixes to python sdk
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- sdks/python/julep/api/client.py
+++ sdks/python/julep/api/client.py
@@ -831,7 +831,7 @@ class JulepApi:
         if min_p is not OMIT:
             _request["min_p"] = min_p
         if preset is not OMIT:
-            _request["preset"] = preset.value
+            _request["preset"] = preset.value  # type: ignore
         if recall is not OMIT:
             _request["recall"] = recall
         if record is not OMIT:
@@ -2596,7 +2596,7 @@ class AsyncJulepApi:
         if min_p is not OMIT:
             _request["min_p"] = min_p
         if preset is not OMIT:
-            _request["preset"] = preset.value
+            _request["preset"] = preset.value  # type: ignore
         if recall is not OMIT:
             _request["recall"] = recall
         if record is not OMIT:
