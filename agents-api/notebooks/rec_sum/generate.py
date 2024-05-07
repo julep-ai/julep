c05575ed1a4ed7293e3fe1e1c9fe3ddf2b4ad500
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-05-07 21:28:23 +0530

    wip
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ agents-api/notebooks/rec_sum/generate.py
@@ -0,0 +1,22 @@
+from openai import AsyncClient
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+
+client = AsyncClient()
+
+
+@retry(wait=wait_fixed(2), stop=stop_after_attempt(5))
+async def generate(
+    messages: list[dict], 
+    client: AsyncClient=client,
+    model: str="gpt-4-turbo",
+    **kwargs
+) -> dict:
+    result = await client.chat.completions.create(
+        model=model, messages=messages, **kwargs
+    )
+    
+    result = result.choices[0].message.__dict__
+
+    return result
+
