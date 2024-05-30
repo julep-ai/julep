7cd1c321f208b6354b18fa2aeb0a40386d39a523
Diwank Tomer <diwank@julep.ai>
2024-05-29 19:31:06 -0700

    wip(agents-api): cozo queries for tasks
    Signed-off-by: Diwank Tomer <diwank@julep.ai>

--- agents-api/agents_api/models/task/get_task.py
+++ agents-api/agents_api/models/task/get_task.py
@@ -4,6 +4,28 @@ from ..utils import cozo_query
 
 
 @cozo_query
-def get_task_query(developer_id: UUID, task_id: UUID) -> tuple[str, dict]:
-    query = """"""
-    return (query, {"developer_id": str(developer_id), "task_id": str(task_id)})
+def get_task_query(agent_id: UUID, task_id: UUID) -> tuple[str, dict]:
+    query = """
+    ?[
+        name,
+        description,
+        input_schema,
+        tools_available,
+        workflows,
+        created_at,
+        updated_at,
+    ] := *tasks {
+            agent_id: to_uuid($agent_id),
+            task_id: to_uuid($task_id),
+            updated_at_ms,
+            name,
+            description,
+            input_schema,
+            tools_available,
+            workflows,
+            created_at,
+            @ 'NOW'
+        }, updated_at = to_int(updated_at_ms) / 1000
+    """
+
+    return (query, {"agent_id": str(agent_id), "task_id": str(task_id)})
--- agents-api/agents_api/models/task/list_tasks.py
+++ agents-api/agents_api/models/task/list_tasks.py
@@ -7,7 +7,7 @@ from ..utils import cozo_query
 
 @cozo_query
 def list_tasks_query(
-    developer_id: UUID,
+    agent_id: UUID,
     limit: int = 100,
     offset: int = 0,
     # metadata_filter: dict[str, Any] = {},
@@ -21,4 +21,32 @@ def list_tasks_query(
     Returns:
         pd.DataFrame: A DataFrame containing the queried task data.
     """
-    pass
+
+    query = """
+    ?[
+        task_id,
+        name,
+        description,
+        input_schema,
+        tools_available,
+        workflows,
+        created_at,
+        updated_at,
+    ] := *tasks {
+            agent_id: to_uuid($agent_id),
+            task_id,
+            updated_at_ms,
+            name,
+            description,
+            input_schema,
+            tools_available,
+            workflows,
+            created_at,
+            @ 'NOW'
+        }, updated_at = to_int(updated_at_ms) / 1000
+
+      :limit $limit
+      :offset $offset
+    """
+
+    return (query, {"agent_id": str(agent_id), "limit": limit, "offset": offset})
