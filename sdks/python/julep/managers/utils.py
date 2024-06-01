0ca1d07766d1438171f2d4e9652f8251741cf335
Diwank Tomer <diwank@julep.ai>
2024-06-01 10:04:20 -0700

    hotfix(sdks/python): Fix bug in model_construct
    Signed-off-by: Diwank Tomer <diwank@julep.ai>

--- sdks/python/julep/managers/utils.py
+++ sdks/python/julep/managers/utils.py
@@ -3,7 +3,9 @@ from functools import wraps
 from typing import Callable
 from uuid import UUID
 from typing_extensions import ParamSpec
+
 from pydantic.main import Model
+
 from ..api.types import ResourceCreatedResponse
 
 
@@ -38,6 +40,21 @@ def is_valid_uuid4(uuid_to_test: str) -> bool:
     return True
 
 
+def pydantic_model_construct(cls: type[Model], **kwargs):
+    import pydantic
+
+    pydantic_version = int(pydantic.__version__[0])
+
+    if pydantic_version < 2:
+        return cls.construct(**kwargs)
+
+    # Is it running in compatibility mode?
+    if pydantic.v1.main.BaseModel in cls.__mro__:
+        return cls.construct(**kwargs)
+
+    return cls.model_construct(**kwargs)
+
+
 def rewrap_in_class(cls: type[Model]):
     def decorator(func: Callable[P, ResourceCreatedResponse]):
         # This wrapper is used for asynchronous functions to ensure they are properly awaited and their results are processed by `cls.construct`.
@@ -51,7 +68,7 @@ def rewrap_in_class(cls: type[Model]):
         def sync_wrapper(*args, **kwargs):
             # Logging at this point might be useful for debugging, but should use a proper logging framework instead of print statements for production code.
             result = func(*args, **kwargs)
-            return cls.model_construct(**kwargs, **result.dict())
+            return pydantic_model_construct(cls, **kwargs, **result.dict())
 
         return async_wrapper if iscoroutinefunction(func) else sync_wrapper
 
