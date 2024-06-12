ea7bbff19f2ae610a702ce94bf2f1d0a3885ed2d
Dmitry Paramonov <asmatic075@gmail.com>
2024-06-12 15:50:51 +0300

    feat: Implement activities

--- agents-api/agents_api/activities/task_steps/__init__.py
+++ agents-api/agents_api/activities/task_steps/__init__.py
@@ -1,5 +1,15 @@
+import celpy
 from temporalio import activity
+from jinja2 import Environment
 
+from agents_api.autogen.openapi_model import (
+    PromptWorkflowStep, 
+    EvaluateWorkflowStep, 
+    YieldWorkflowStep,
+    ToolCallWorkflowStep,
+    ErrorWorkflowStep,
+    IfElseWorkflowStep,
+)
 from ...common.protocol.tasks import (
     StepContext,
 )
@@ -7,37 +17,67 @@ from ...common.protocol.tasks import (
 
 @activity.defn
 async def prompt_step(context: StepContext) -> dict:
-    print("Running prompt step")
+    # TODO: get the template string somehow
+    template_str = "..."
+    if not isinstance(context.definition, PromptWorkflowStep):
+        return {}
+    
+    env = Environment()
+    template = env.from_string(template_str)
+    prompt = template.render(prompt=context.definition.prompt)
+
     return {"test": "result"}
 
 
 @activity.defn
 async def evaluate_step(context: StepContext) -> dict:
-    print("Running prompt step")
-    return {"test": "result"}
+    if not isinstance(context.definition, EvaluateWorkflowStep):
+        return {}
+    
+    #FIXME: set the field to keep source code
+    source: str = context.definition.evaluate
+    env = celpy.Environment()
+    ast = env.compile(source)
+    prog = env.program(ast)
+    # TODO: set args
+    args = {}
+    result = prog.evaluate(args)
+    return {"result": result}
 
 
 @activity.defn
 async def yield_step(context: StepContext) -> dict:
-    print("Running yield step")
+    if not isinstance(context.definition, YieldWorkflowStep):
+        return {}
+    
+    #TODO: implement
+
     return {"test": "result"}
 
 
 @activity.defn
 async def tool_call_step(context: StepContext) -> dict:
-    print("Running tool_call step")
+    if not isinstance(context.definition, ToolCallWorkflowStep):
+        return {}
+
+    #TODO: implement
+
     return {"test": "result"}
 
 
 @activity.defn
 async def error_step(context: StepContext) -> dict:
-    print("Running error step")
-    return {"test": "result"}
+    if not isinstance(context.definition, ErrorWorkflowStep):
+        return {}
+
+    return {"error": context.definition.error}
 
 
 @activity.defn
 async def if_else_step(context: StepContext) -> dict:
-    print("Running if_else step")
+    if not isinstance(context.definition, IfElseWorkflowStep):
+        return {}
+
     return {"test": "result"}
 
 
