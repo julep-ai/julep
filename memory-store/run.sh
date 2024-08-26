f8860d8fa1fec7da0632bb4827fa0e3fa5cd6504
Diwank Tomer <diwank@julep.ai>
2024-08-26 19:52:18 -0400

    feat(memory-store): Add backup cronjob
    Signed-off-by: Diwank Tomer <diwank@julep.ai>

--- memory-store/run.sh
+++ memory-store/run.sh
@@ -12,9 +12,8 @@ export COZO_ROCKSDB_DIR=${COZO_ROCKSDB_DIR:-cozo.db}
 echo $COZO_AUTH_TOKEN > $MNT_DIR/${COZO_ROCKSDB_DIR}.rocksdb.cozo_auth
 
 # Start server
-${APP_HOME:=.}/bin/cozo-bin server \
+${APP_HOME:=.}/bin/cozo server \
     --engine rocksdb \
     --path $MNT_DIR/${COZO_ROCKSDB_DIR} \
     --bind 0.0.0.0 \
-    --port ${COZO_PORT:=9070} \
-    --token-table __tokens
+    --port ${COZO_PORT:=9070}
