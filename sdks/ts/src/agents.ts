0a02413b70063e56cae001c0a6f4669571098992
Dmitry Paramonov <asmatic075@gmail.com>
2024-08-28 22:08:37 +0300

    feat: Add more agents routes

--- sdks/ts/src/agents.ts
+++ sdks/ts/src/agents.ts
@@ -1,9 +1,20 @@
-import { Agents_CreateAgentRequest, Common_uuid, Agents_UpdateAgentRequest, Common_limit, Common_offset, Agents_PatchAgentRequest, Docs_VectorDocSearchRequest, Docs_TextOnlyDocSearchRequest, Docs_HybridDocSearchRequest } from "./api";
-import { JulepApiClient } from "./api/JulepApiClient";
+import {
+    Agents_CreateAgentRequest,
+    Common_uuid,
+    Agents_UpdateAgentRequest,
+    Common_limit,
+    Common_offset,
+    Agents_PatchAgentRequest,
+    Docs_VectorDocSearchRequest,
+    Docs_TextOnlyDocSearchRequest,
+    Docs_HybridDocSearchRequest,
+    Docs_CreateDocRequest,
+    Tools_PatchToolRequest,
+    Tools_UpdateToolRequest
+} from "./api";
+import { BaseRoutes } from "./baseRoutes";
 
-export class AgentsRoutes {
-    constructor(private apiClient: JulepApiClient) { }
-
+export class AgentsRoutes extends BaseRoutes{
     async create({ requestBody }: { requestBody: Agents_CreateAgentRequest }) {
         return await this.apiClient.default.agentsRouteCreate({ requestBody })
     }
@@ -60,5 +71,90 @@ export class AgentsRoutes {
         };
     }) {
         return await this.apiClient.default.agentsDocsSearchRouteSearch({ id, requestBody })
+    }
+
+    async createDoc({ id, requestBody }: {
+        id: Common_uuid;
+        requestBody: Docs_CreateDocRequest;
+    }) {
+        return await this.apiClient.default.agentDocsRouteCreate({ id, requestBody })
+    }
+
+    async listDocs({
+        id,
+        limit = 100,
+        offset,
+        sortBy = "created_at",
+        direction = "asc",
+        metadataFilter = "{}",
+    }: {
+        id: Common_uuid;
+        limit?: Common_limit;
+        offset: Common_offset;
+        sortBy?: "created_at" | "updated_at";
+        direction?: "asc" | "desc";
+        metadataFilter?: string;
+    }) {
+        return await this.apiClient.default.agentDocsRouteList({ id, limit, offset, sortBy, direction, metadataFilter })
+    }
+
+    async createTool({ id, requestBody }: {
+        id: Common_uuid;
+        requestBody: Agents_CreateAgentRequest;
+    }) {
+        return await this.apiClient.default.agentToolsRouteCreate({ id, requestBody })
+    }
+
+    async deleteTool({ id, childId }: {
+        id: Common_uuid;
+        childId: Common_uuid;
+    }) {
+        return await this.apiClient.default.agentToolsRouteDelete({ id, childId })
+    }
+
+    async listTools(
+        {
+            id,
+            limit = 100,
+            offset,
+            sortBy = "created_at",
+            direction = "asc",
+            metadataFilter = "{}",
+        }: {
+            id: Common_uuid;
+            limit?: Common_limit;
+            offset: Common_offset;
+            sortBy?: "created_at" | "updated_at";
+            direction?: "asc" | "desc";
+            metadataFilter?: string;
+        }
+    ) {
+        return await this.apiClient.default.agentToolsRouteList({ id, limit, offset, sortBy, direction, metadataFilter })
+    }
+
+    async patchTool({
+        id,
+        childId,
+        requestBody,
+    }: {
+        id: Common_uuid;
+        childId: Common_uuid;
+        requestBody: Tools_PatchToolRequest;
+    }) {
+        return await this.apiClient.default.agentToolsRoutePatch({ id, childId, requestBody })
+    }
+
+    async updateTool(
+        {
+            id,
+            childId,
+            requestBody,
+        }: {
+            id: Common_uuid;
+            childId: Common_uuid;
+            requestBody: Tools_UpdateToolRequest;
+        }
+    ) {
+        return await this.apiClient.default.agentToolsRouteUpdate({ id, childId, requestBody })
     }
 }
