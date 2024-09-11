b4f3ad78fed515238da7bfef44db1f7f9ae72e93
Dmitry Paramonov <asmatic075@gmail.com>
2024-09-11 23:00:43 +0300

    feat: Add dialog entries summarization

--- agents-api/agents_api/activities/summarization.py
+++ agents-api/agents_api/activities/summarization.py
@@ -1,86 +1,77 @@
 #!/usr/bin/env python3
 
 
-import pandas as pd
+import asyncio
+from uuid import UUID
+
 from beartype import beartype
 from temporalio import activity
 
-# from agents_api.models.entry.entries_summarization import (
-#     entries_summarization_query,
-#     get_toplevel_entries_query,
-# )
-
-
-# TODO: Implement entry summarization queries
-# SCRUM-3
-def entries_summarization_query(*args, **kwargs) -> pd.DataFrame:
-    return pd.DataFrame()
-
-
-def get_toplevel_entries_query(*args, **kwargs) -> pd.DataFrame:
-    return pd.DataFrame()
-
-
-# TODO: Implement entry summarization activities
-# SCRUM-4
+from agents_api.autogen.openapi_model import Entry
+from agents_api.env import summarization_model_name
+from agents_api.models.entry.entries_summarization import (
+    entries_summarization_query,
+    get_toplevel_entries_query,
+)
+from agents_api.rec_sum.entities import get_entities
+from agents_api.rec_sum.summarize import summarize_messages
+from agents_api.rec_sum.trim import trim_messages
 
 
 @activity.defn
 @beartype
 async def summarization(session_id: str) -> None:
-    raise NotImplementedError()
+    session_id = UUID(session_id)
+    entries = []
+    entities_entry_ids = []
+    for _, row in get_toplevel_entries_query(session_id=session_id).iterrows():
+        if row["role"] == "system" and row.get("name") == "entities":
+            entities_entry_ids.append(UUID(row["entry_id"], version=4))
+        else:
+            entries.append(row)
 
-    # session_id = UUID(session_id)
-    # entries = []
-    # entities_entry_ids = []
-    # for _, row in get_toplevel_entries_query(session_id=session_id).iterrows():
-    #     if row["role"] == "system" and row.get("name") == "entities":
-    #         entities_entry_ids.append(UUID(row["entry_id"], version=4))
-    #     else:
-    #         entries.append(row)
+    assert len(entries) > 0, "no need to summarize on empty entries list"
 
-    # assert len(entries) > 0, "no need to summarize on empty entries list"
+    summarized, entities = await asyncio.gather(
+        summarize_messages(entries, model=summarization_model_name),
+        get_entities(entries, model=summarization_model_name),
+    )
+    trimmed_messages = await trim_messages(summarized, model=summarization_model_name)
+    ts_delta = (entries[1]["timestamp"] - entries[0]["timestamp"]) / 2
+    new_entities_entry = Entry(
+        session_id=session_id,
+        source="summarizer",
+        role="system",
+        name="entities",
+        content=entities["content"],
+        timestamp=entries[0]["timestamp"] + ts_delta,
+    )
 
-    # summarized, entities = await asyncio.gather(
-    #     summarize_messages(entries, model=summarization_model_name),
-    #     get_entities(entries, model=summarization_model_name),
-    # )
-    # trimmed_messages = await trim_messages(summarized, model=summarization_model_name)
-    # ts_delta = (entries[1]["timestamp"] - entries[0]["timestamp"]) / 2
-    # new_entities_entry = Entry(
-    #     session_id=session_id,
-    #     source="summarizer",
-    #     role="system",
-    #     name="entities",
-    #     content=entities["content"],
-    #     timestamp=entries[0]["timestamp"] + ts_delta,
-    # )
+    entries_summarization_query(
+        session_id=session_id,
+        new_entry=new_entities_entry,
+        old_entry_ids=entities_entry_ids,
+    )
 
-    # entries_summarization_query(
-    #     session_id=session_id,
-    #     new_entry=new_entities_entry,
-    #     old_entry_ids=entities_entry_ids,
-    # )
+    trimmed_map = {
+        m["index"]: m["content"] for m in trimmed_messages if m.get("index") is not None
+    }
 
-    # trimmed_map = {
-    #     m["index"]: m["content"] for m in trimmed_messages if m.get("index") is not None
-    # }
+    for idx, msg in enumerate(summarized):
+        new_entry = Entry(
+            session_id=session_id,
+            source="summarizer",
+            role="system",
+            name="information",
+            content=trimmed_map.get(idx, msg["content"]),
+            timestamp=entries[-1]["timestamp"] + 0.01,
+        )
 
-    # for idx, msg in enumerate(summarized):
-    #     new_entry = Entry(
-    #         session_id=session_id,
-    #         source="summarizer",
-    #         role="system",
-    #         name="information",
-    #         content=trimmed_map.get(idx, msg["content"]),
-    #         timestamp=entries[-1]["timestamp"] + 0.01,
-    #     )
-
-    #     entries_summarization_query(
-    #         session_id=session_id,
-    #         new_entry=new_entry,
-    #         old_entry_ids=[
-    #             UUID(entries[idx - 1]["entry_id"], version=4)
-    #             for idx in msg["summarizes"]
-    #         ],
-    #     )
+        entries_summarization_query(
+            session_id=session_id,
+            new_entry=new_entry,
+            old_entry_ids=[
+                UUID(entries[idx - 1]["entry_id"], version=4)
+                for idx in msg["summarizes"]
+            ],
+        )
