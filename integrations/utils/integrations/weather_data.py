619e182c184e20dbf33ca053e49f57675e71bc7f
HamadaSalhab <hamada.a.salhab@gmail.com>
2024-09-25 02:24:58 +0300

    add weather_data integration

---
+++ integrations/utils/integrations/weather_data.py
@@ -0,0 +1,15 @@
+import os
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+
+
+async def weather_data(parameters: dict) -> str:
+    location = parameters.get("location")
+
+    openweathermap_api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+    if not location:
+        raise ValueError("Location parameter is required for weather data")
+
+    weather = OpenWeatherMapAPIWrapper(
+        openweathermap_api_key=openweathermap_api_key)
+
+    return weather.run(location)
