2fb627c0e8fc80a9349d855313573f9d71df6a03
HamadaSalhab <hamada.a.salhab@gmail.com>
2024-09-25 18:33:15 +0300

    add dummy send mail integration

---
+++ integrations-service/integrations/utils/integrations/gmail/send_mail.py
@@ -0,0 +1,6 @@
+async def send_mail(parameters: dict) -> str:
+    """
+    Dummy integration for sending an email to a specified recipient with a given subject and message.
+    """
+
+    return "Mail sent"
