a1fb6aada65702fe1af006eabd4697f2eff92d68
HamadaSalhab <hamada.a.salhab@gmail.com>
2024-10-02 22:41:49 +0300

    Add more nont-retriable errors

--- agents-api/agents_api/common/retry_policies.py
+++ agents-api/agents_api/common/retry_policies.py
@@ -1,12 +1,12 @@
 from datetime import timedelta
 
 from temporalio.common import RetryPolicy
-
+from ..env import debug, testing
 DEFAULT_RETRY_POLICY = RetryPolicy(
     initial_interval=timedelta(seconds=1),
     backoff_coefficient=2,
-    maximum_attempts=2,
-    maximum_interval=timedelta(seconds=10),
+    maximum_attempts=2 if debug or testing else 25,
+    maximum_interval=timedelta(seconds=10) if debug or testing else timedelta(seconds=300),
     non_retryable_error_types=[
         "WorkflowExecutionAlreadyStarted",
         "TypeError",
@@ -14,5 +14,20 @@ DEFAULT_RETRY_POLICY = RetryPolicy(
         "HTTPException",
         "SyntaxError",
         "ValueError",
+        "ZeroDivisionError",
+        "jinja2.exceptions.TemplateSyntaxError",
+        "jinja2.exceptions.TemplateNotFound",
+        "jsonschema.exceptions.ValidationError",
+        "pydantic.ValidationError",
+        "asyncio.CancelledError",
+        "asyncio.InvalidStateError",
+        "requests.exceptions.InvalidURL",
+        "requests.exceptions.MissingSchema",
+        "temporalio.exceptions.TerminalFailure",
+        "temporalio.exceptions.CanceledError",
+        "fastapi.exceptions.HTTPException",
+        "fastapi.exceptions.RequestValidationError",
+        "httpx.RequestError",
+        "httpx.HTTPStatusError",
     ],
 )
