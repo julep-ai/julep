bb7aa698af871cf2b62dab4bdcd4d86e899910f7
Vedantsahai18 <Vedantsahai18@users.noreply.github.com>
2024-10-15 22:42:50 +0000

    refactor: Lint agents-api (CI)

--- agents-api/agents_api/common/utils/yaml.py
+++ agents-api/agents_api/common/utils/yaml.py
@@ -7,5 +7,6 @@ import yaml
 def load(string: str) -> Any:
     return yaml.load(string, Loader=yaml.CSafeLoader)
 
+
 def dump(value: Any) -> str:
     return yaml.dump(value, Dumper=yaml.CSafeDumper)
