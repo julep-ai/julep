5a25b84e559ebad43a87fb51a510f9a00793eac8
Ahmad-mtos <ahmad.haidar.syr@gmail.com>
2024-10-28 18:41:55 +0300

    Fix: fix fetching the error message

--- agents-api/agents_api/activities/task_steps/base_evaluate.py
+++ agents-api/agents_api/activities/task_steps/base_evaluate.py
@@ -9,18 +9,24 @@ from temporalio import activity
 from ...common.storage_handler import auto_blob_store
 from ...env import testing
 from ..utils import get_evaluator
+from simpleeval import NameNotDefined
 from thefuzz import fuzz
 
 class EvaluateError(Exception):
     def __init__(self, error, expression, values):
-        message = error.message
-        if "unhashable" in error.message and "{{" in expression:
+        error_message = error.message if hasattr(error, "message") else str(error)
+        message = error_message
+
+        # Catch a possible jinja template error
+        if "unhashable" in error_message and "{{" in expression:
             message += "\nSuggestion: It seems like you used a jinja template, did you mean to use a python expression?"
-        if isinstance(error, NameError):
-            misspelledName = error.message.split("'")[1]
+        
+        # Catch a possible misspell in a variable name
+        if isinstance(error, NameNotDefined):
+            misspelledName = error_message.split("'")[1]
             for variableName in values.keys():
                 if fuzz.ratio(variableName, misspelledName) >= 90.0:
-                    message += f"\nDid you mean {variableName} instead of {misspelledName}?"
+                    message += f"\nDid you mean '{variableName}' instead of '{misspelledName}'?"
         super().__init__(message)
 
 @auto_blob_store
