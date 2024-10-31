2d83db3597abbb4194d96ea9173347f3ed279038
vedantsahai18 <vedantsahai18@gmail.com>
2024-10-30 21:06:58 -0400

    feat(agents-api): bug fixes for the prompt step and types of tool call fixed

--- agents-api/agents_api/activities/task_steps/prompt_step.py
+++ agents-api/agents_api/activities/task_steps/prompt_step.py
@@ -1,6 +1,6 @@
 import os
 
-from anthropic import Anthropic  # Import Anthropic client
+from anthropic import AsyncAnthropic, Anthropic  # Import AsyncAnthropic client
 from beartype import beartype
 from temporalio import activity
 from temporalio.exceptions import ApplicationError
@@ -9,6 +9,7 @@ from ...autogen.Tools import Tool
 from ...clients import (
     litellm,  # We dont directly import `acompletion` so we can mock it
 )
+from litellm.utils import CustomStreamWrapper, ModelResponse
 from ...common.protocol.tasks import StepContext, StepOutcome
 from ...common.storage_handler import auto_blob_store
 from ...common.utils.template import render_template
@@ -20,7 +21,7 @@ COMPUTER_USE_BETA_FLAG = "computer-use-2024-10-22"
 
 # FIXME: This shouldn't be here.
 def format_agent_tool(tool: Tool) -> dict:
-    if tool.function:
+    if tool.type == "function":
         return {
             "type": "function",
             "function": {
@@ -29,20 +30,20 @@ def format_agent_tool(tool: Tool) -> dict:
                 "parameters": tool.function.parameters,
             },
         }
-    elif tool.computer_20241022:
+    elif tool.type == "computer_20241022":
         return {
             "type": tool.type,
             "name": tool.name,
-            "display_width_px": tool.display_width_px,
-            "display_height_px": tool.display_height_px,
-            "display_number": tool.display_number,
+            "display_width_px": tool.spec['display_width_px'],
+            "display_height_px": tool.spec['display_height_px'],
+            "display_number": tool.spec['display_number'],
         }
-    elif tool.bash_20241022:
+    elif tool.type == "bash_20241022":
         return {
             "type": tool.type,
             "name": tool.name,
         }
-    elif tool.text_editor_20241022:
+    elif tool.type == "text_editor_20241022":
         return {
             "type": tool.type,
             "name": tool.name,
@@ -50,7 +51,6 @@ def format_agent_tool(tool: Tool) -> dict:
     # TODO: Add integration | system | api_call tool types
     else:
         return {}
-
 
 @activity.defn
 @auto_blob_store
@@ -86,7 +86,7 @@ async def prompt_step(context: StepContext) -> StepOutcome:
         sort_by="created_at",
         direction="desc",
     )
-
+    # grab the tools from context.current_step.tools and then append it to the agent_tools
     if context.current_step.settings:
         passed_settings: dict = context.current_step.settings.model_dump(
             exclude_unset=True
@@ -99,25 +99,38 @@ async def prompt_step(context: StepContext) -> StepOutcome:
         prompt = [{"role": "user", "content": prompt}]
 
     # Format agent_tools for litellm
+    # Initialize the formatted_agent_tools with context tools
+    task_tools = context.tools
     formatted_agent_tools = [
-        format_agent_tool(tool) for tool in agent_tools if format_agent_tool(tool)
+        format_agent_tool(tool) for tool in task_tools
     ]
+    # Add agent_tools if they are not already in formatted_agent_tools
+    for agent_tool in agent_tools:
+        if format_agent_tool(agent_tool) and format_agent_tool(agent_tool) not in formatted_agent_tools:
+            formatted_agent_tools.append(format_agent_tool(agent_tool))
 
     # Check if the model is Anthropic
-    if "claude-3.5-sonnet-20241022" == agent_model.lower():
+    if (agent_model.lower().startswith("claude-3.5")
+        and any(
+            tool["type"] in ["computer_20241022", "bash_20241022", "text_editor_20241022"]
+            for tool in formatted_agent_tools)
+        ):
         # Retrieve the API key from the environment variable
         betas = [COMPUTER_USE_BETA_FLAG]
         # Use Anthropic API directly
-        client = Anthropic(api_key=anthropic_api_key)
-
+        client = AsyncAnthropic(api_key=anthropic_api_key)
+        new_prompt = [{"role": "user", "content": prompt[0]["content"]}]
         # Claude Response
         response = await client.beta.messages.create(
-            model=agent_model,
-            messages=prompt,
+            model="claude-3-5-sonnet-20241022",
+            messages=new_prompt,
             tools=formatted_agent_tools,
             max_tokens=1024,
             betas=betas,
         )
+        print("-" * 100)
+        # print("Model Response: ", model_response)
+        print("Response: ", response)
 
         return StepOutcome(
             output=response.model_dump()
