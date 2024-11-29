9ce2d6b0bf03408f288fdeb49a82eda04b3699ee
Dmitry Paramonov <asmatic075@gmail.com>
2024-11-29 13:56:56 +0300

    fix: Await a coroutine

--- agents-api/agents_api/metrics/counters.py
+++ agents-api/agents_api/metrics/counters.py
@@ -24,7 +24,7 @@ def increase_counter(metric_label: str, id_field_name: str = "developer_id"):
         @wraps(func)
         async def async_wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
             metric.labels(kwargs.get(id_field_name, "not_set")).inc()
-            return func(*args, **kwargs)
+            return await func(*args, **kwargs)
 
         return async_wrapper if inspect.iscoroutinefunction(func) else wrapper
 
