17c9608bb4ca97883e2e037ae3d66c169186d314
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-12-06 21:55:29 +0530

    feat(memory-store): integrate the new cozo rocksdb

--- memory-store/run.sh
+++ memory-store/run.sh
@@ -9,14 +9,14 @@ mkdir -p ${COZO_MNT_DIR:=/data}/${COZO_ROCKSDB_DIR:-cozo.db}
 export COZO_AUTH_TOKEN=${COZO_AUTH_TOKEN:=`tr -dc A-Za-z0-9 </dev/urandom | head -c 72`}
 echo "Auth token: $COZO_AUTH_TOKEN"
 export COZO_ROCKSDB_DIR=${COZO_ROCKSDB_DIR:-cozo.db}
-echo $COZO_AUTH_TOKEN > $COZO_MNT_DIR/${COZO_ROCKSDB_DIR}.rocksdb.cozo_auth
+echo $COZO_AUTH_TOKEN > $COZO_MNT_DIR/${COZO_ROCKSDB_DIR}.newrocksdb.cozo_auth
 
 # Copy options file to the RocksDB directory
-cp /app/options $COZO_MNT_DIR/${COZO_ROCKSDB_DIR}/options
+cp /app/options $COZO_MNT_DIR/${COZO_ROCKSDB_DIR}/OPTIONS-000007
 
 # Start server
 ${APP_HOME:=.}/bin/cozo server \
-    --engine rocksdb \
+    --engine newrocksdb \
     --path $COZO_MNT_DIR/${COZO_ROCKSDB_DIR} \
     --bind 0.0.0.0 \
     --port ${COZO_PORT:=9070}
