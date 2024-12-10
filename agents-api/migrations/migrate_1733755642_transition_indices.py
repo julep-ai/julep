74595f10cba5631cd6f3f6f00100f474a2aa7a37
Dmitry Paramonov <asmatic075@gmail.com>
2024-12-10 10:05:31 +0300

    feat: Add metadata index

--- agents-api/migrations/migrate_1733755642_transition_indices.py
+++ agents-api/migrations/migrate_1733755642_transition_indices.py
@@ -8,10 +8,12 @@ create_transition_indices = dict(
     up="""
     ::index create executions:execution_id_status_idx { status, execution_id }
     ::index create transitions:execution_id_type_idx { type, transition_id }
+    ::index create executions:execution_id_metadata_idx { metadata, execution_id }
     """,
     down="""
     ::index drop executions:execution_id_status_idx
     ::index drop transitions:execution_id_type_idx
+    ::index drop executions:execution_id_metadata_idx
     """,
 )
 
