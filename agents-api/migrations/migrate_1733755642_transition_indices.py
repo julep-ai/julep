e8f1d87297414c54e39fcd2cfb1780c7cd415742
Dmitry Paramonov <asmatic075@gmail.com>
2024-12-10 10:11:37 +0300

    fix: Fix index field

--- agents-api/migrations/migrate_1733755642_transition_indices.py
+++ agents-api/migrations/migrate_1733755642_transition_indices.py
@@ -7,7 +7,7 @@ CREATED_AT = 1733755642.881131
 create_transition_indices = dict(
     up="""
     ::index create executions:execution_id_status_idx { status, execution_id }
-    ::index create transitions:execution_id_type_idx { type, transition_id }
+    ::index create transitions:execution_id_type_idx { type, execution_id }
     ::index create executions:execution_id_metadata_idx { metadata, execution_id }
     """,
     down="""
