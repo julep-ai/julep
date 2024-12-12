24e9afb34fac345349d2cf2333f5b4786b2cb8c0
creatorrr <creatorrr@users.noreply.github.com>
2024-12-12 06:45:26 +0000

    refactor: Lint agents-api (CI)

--- agents-api/migrations/migrate_1733985509_add_columns_to_indices.py
+++ agents-api/migrations/migrate_1733985509_add_columns_to_indices.py
@@ -1,7 +1,8 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 
 MIGRATION_ID = "add_columns_to_indices"
 CREATED_AT = 1733985509.241258
+
 
 def up(client):
     client.run(
@@ -12,6 +13,7 @@ def up(client):
         { ::index create docs:owner_id_metadata_doc_id_idx { owner_type, owner_id, metadata, doc_id } }
         """
     )
+
 
 def down(client):
     client.run(
