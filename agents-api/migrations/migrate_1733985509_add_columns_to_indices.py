046236912faa5119965050c04ed8f1d9752f8a65
Diwank Singh Tomer <diwank.singh@gmail.com>
2024-12-12 12:14:14 +0530

    feat(agents-api): Add missing columns to indices
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ agents-api/migrations/migrate_1733985509_add_columns_to_indices.py
@@ -0,0 +1,24 @@
+#/usr/bin/env python3
+
+MIGRATION_ID = "add_columns_to_indices"
+CREATED_AT = 1733985509.241258
+
+def up(client):
+    client.run(
+        """
+        { ::index drop executions:execution_id_status_idx }
+        { ::index create executions:execution_id_status_idx { task_id, execution_id, status } }
+        { ::index drop docs:owner_id_metadata_doc_id_idx }
+        { ::index create docs:owner_id_metadata_doc_id_idx { owner_type, owner_id, metadata, doc_id } }
+        """
+    )
+
+def down(client):
+    client.run(
+        """
+        { ::index drop executions:execution_id_status_idx }
+        { ::index create executions:execution_id_status_idx { execution_id, status } }
+        { ::index drop docs:owner_id_metadata_doc_id_idx }
+        { ::index create docs:owner_id_metadata_doc_id_idx { owner_id, metadata, doc_id } }
+        """
+    )
