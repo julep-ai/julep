e84bcd66573b14fdcb53fcf981f773d4076909d0
Dmitry Paramonov <asmatic075@gmail.com>
2024-12-14 22:43:10 +0300

    fix: Call get_developer asynchronously

--- agents-api/agents_api/dependencies/developer_id.py
+++ agents-api/agents_api/dependencies/developer_id.py
@@ -36,7 +36,9 @@ async def get_developer_data(
         assert (
             not x_developer_id
         ), "X-Developer-Id header not allowed in multi-tenant mode"
-        return get_developer(developer_id=UUID("00000000-0000-0000-0000-000000000000"))
+        return await get_developer(
+            developer_id=UUID("00000000-0000-0000-0000-000000000000")
+        )
 
     if not x_developer_id:
         raise InvalidHeaderFormat("X-Developer-Id header required")
@@ -47,6 +49,6 @@ async def get_developer_data(
         except ValueError as e:
             raise InvalidHeaderFormat("X-Developer-Id must be a valid UUID") from e
 
-    developer = get_developer(developer_id=x_developer_id)
+    developer = await get_developer(developer_id=x_developer_id)
 
     return developer
