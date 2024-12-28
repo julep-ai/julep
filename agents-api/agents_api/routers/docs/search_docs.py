ff8e9bdf7beafdfb4f297549835c1a51f6aa5b57
HamadaSalhab <hamada.a.salhab@gmail.com>
2024-12-28 03:14:33 +0300

    Fix: Wrong await usage

--- agents-api/agents_api/routers/docs/search_docs.py
+++ agents-api/agents_api/routers/docs/search_docs.py
@@ -20,7 +20,7 @@ from ...queries.docs.search_docs_hybrid import search_docs_hybrid
 from .router import router
 
 
-async def get_search_fn_and_params(
+def get_search_fn_and_params(
     search_params,
 ) -> Tuple[
     Any, Optional[Dict[str, Union[float, int, str, Dict[str, float], List[float]]]]
@@ -31,7 +31,7 @@ async def get_search_fn_and_params(
         case TextOnlyDocSearchRequest(
             text=query, limit=k, metadata_filter=metadata_filter
         ):
-            search_fn = await search_docs_by_text
+            search_fn = search_docs_by_text
             params = dict(
                 query=query,
                 k=k,
@@ -44,7 +44,7 @@ async def get_search_fn_and_params(
             confidence=confidence,
             metadata_filter=metadata_filter,
         ):
-            search_fn = await search_docs_by_embedding
+            search_fn = search_docs_by_embedding
             params = dict(
                 query_embedding=query_embedding,
                 k=k * 3 if search_params.mmr_strength > 0 else k,
@@ -60,7 +60,7 @@ async def get_search_fn_and_params(
             alpha=alpha,
             metadata_filter=metadata_filter,
         ):
-            search_fn = await search_docs_hybrid
+            search_fn = search_docs_hybrid
             params = dict(
                 query=query,
                 query_embedding=query_embedding,
@@ -94,10 +94,10 @@ async def search_user_docs(
     """
 
     # MMR here
-    search_fn, params = await get_search_fn_and_params(search_params)
+    search_fn, params = get_search_fn_and_params(search_params)
 
     start = time.time()
-    docs: list[DocReference] = search_fn(
+    docs: list[DocReference] = await search_fn(
         developer_id=x_developer_id,
         owners=[("user", user_id)],
         **params,
@@ -145,10 +145,10 @@ async def search_agent_docs(
         DocSearchResponse: The search results.
     """
 
-    search_fn, params = await get_search_fn_and_params(search_params)
+    search_fn, params = get_search_fn_and_params(search_params)
 
     start = time.time()
-    docs: list[DocReference] = search_fn(
+    docs: list[DocReference] = await search_fn(
         developer_id=x_developer_id,
         owners=[("agent", agent_id)],
         **params,
