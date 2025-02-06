7f5f3525fd1aa626cbc194fe0fbd2c747ada445b
Ahmad Haidar <ahmad.haidar.syr@gmail.com>
2025-02-06 16:37:00 +0300

    fix(agents-api): fetch workflow inputs properly

--- agents-api/agents_api/common/protocol/tasks.py
+++ agents-api/agents_api/common/protocol/tasks.py
@@ -3,6 +3,7 @@ from typing import Annotated, Any, Literal
 from temporalio import workflow
 from temporalio.exceptions import ApplicationError
 
+from ..utils.workflows import get_workflow_name
 with workflow.unsafe.imports_passed_through():
     from pydantic import BaseModel, Field, computed_field
     from pydantic_partial import create_partial_model
@@ -228,13 +229,14 @@ class StepContext(BaseModel):
         assert len(transitions) > 0, "No transitions found"
         inputs = []
         labels = []
-        workflow = transitions[-1].current.workflow
+        workflow = await get_workflow_name(transitions[-1])
         transitions = [
             t
             for t in transitions
-            if t.current.workflow == workflow or t.current.workflow.startswith(f"`{workflow}`")
+            if await get_workflow_name(t) == workflow
         ]
         for transition in transitions:
+            # NOTE: The length hack should be refactored in case we want to implement multi-step control steps
             if transition.next and transition.next.step >= len(inputs):
                 inputs.append(transition.output)
                 labels.append(transition.step_label)
