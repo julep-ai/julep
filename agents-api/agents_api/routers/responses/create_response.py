0f1124d3b9e77c08c7a7b046df21717b9b681917
Ahmad-mtos <Ahmad-mtos@users.noreply.github.com>
2025-03-13 13:45:44 +0000

    refactor: Lint agents-api (CI)

--- agents-api/agents_api/routers/responses/create_response.py
+++ agents-api/agents_api/routers/responses/create_response.py
@@ -14,7 +14,17 @@ async def create_response(
     create_response_data: CreateResponse,
 ) -> Response:
     print("Got create_response_data", create_response_data)
-    return Response(id="resp_67ccd2bed1ec8190b14f964abc0542670bb6a6b452d3795b", object="response", created_at=1741476542, status="completed", error=None, incomplete_details=None, instructions=None, max_output_tokens=None, model="gpt-4o-2024-08-06", output=[
+    return Response(
+        id="resp_67ccd2bed1ec8190b14f964abc0542670bb6a6b452d3795b",
+        object="response",
+        created_at=1741476542,
+        status="completed",
+        error=None,
+        incomplete_details=None,
+        instructions=None,
+        max_output_tokens=None,
+        model="gpt-4o-2024-08-06",
+        output=[
             {
                 "type": "message",
                 "id": "msg_67ccd2bf17f0819081ff3bb2cf6508e60bb6a6b452d3795b",
@@ -28,10 +38,24 @@ async def create_response(
                     }
                 ],
             }
-        ], parallel_tool_calls=True, previous_response_id=None, reasoning={"effort": None, "summary": None}, store=True, temperature=1.0, text={"format": {"type": "text"}}, tool_choice="auto", tools=[], top_p=1.0, truncation="disabled", usage={
+        ],
+        parallel_tool_calls=True,
+        previous_response_id=None,
+        reasoning={"effort": None, "summary": None},
+        store=True,
+        temperature=1.0,
+        text={"format": {"type": "text"}},
+        tool_choice="auto",
+        tools=[],
+        top_p=1.0,
+        truncation="disabled",
+        usage={
             "input_tokens": 36,
             "input_tokens_details": {"cached_tokens": 0},
             "output_tokens": 87,
             "output_tokens_details": {"reasoning_tokens": 0},
             "total_tokens": 123,
-        }, user=None, metadata={})
+        },
+        user=None,
+        metadata={},
+    )
