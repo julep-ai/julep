533f9f4ba9948ef61960678c834cb9745dc27ab4
Dmitry Paramonov <asmatic075@gmail.com>
2025-04-15 15:37:22 +0300

    chore: Remove an endpoint for getting a single secret

--- agents-api/agents_api/queries/secrets/__init__.py
+++ agents-api/agents_api/queries/secrets/__init__.py
@@ -2,14 +2,12 @@
 
 from .create import create_secret
 from .delete import delete_secret
-from .get import get_secret
 from .list import list_secrets
 from .update import update_secret
 
 __all__ = [
     "create_secret",
     "delete_secret",
-    "get_secret",
     "list_secrets",
     "update_secret",
 ]
