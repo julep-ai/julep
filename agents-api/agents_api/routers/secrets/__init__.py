04c219a8596ceac760747f29965e430e6c04b3f8
Dmitry Paramonov <asmatic075@gmail.com>
2025-04-15 15:37:22 +0300

    chore: Remove an endpoint for getting a single secret

--- agents-api/agents_api/routers/secrets/__init__.py
+++ agents-api/agents_api/routers/secrets/__init__.py
@@ -2,8 +2,6 @@ from .create_secret import create_agent_secret as create_agent_secret
 from .create_secret import create_developer_secret as create_developer_secret
 from .delete_secret import delete_agent_secret as delete_agent_secret
 from .delete_secret import delete_developer_secret as delete_developer_secret
-from .get_secret import get_agent_secret as get_agent_secret
-from .get_secret import get_developer_secret as get_developer_secret
 from .list_secrets import list_agent_secrets as list_agent_secrets
 from .list_secrets import list_developer_secrets as list_developer_secrets
 from .router import router as router
